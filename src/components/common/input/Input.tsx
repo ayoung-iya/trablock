f255a6ed0653fe9708eb9535a69ac0118211b7d3
ayoung-iya <rhaehfdl1879@naver.com>
2024-05-30 00:42:16 +0900

    feat: forwardRef 사용 및 라벨 추가

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,9 +1,16 @@
-import { InputHTMLAttributes } from 'react';
+import { ForwardedRef, InputHTMLAttributes, forwardRef } from 'react';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
-
-export default function Input(props: InputProps) {
-  {
-    return <input className="outline-none" {...props} />;
-  }
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
 }
+
+const Input = forwardRef(function Input({ label, id, ...rest }: InputProps, ref: ForwardedRef<HTMLInputElement>) {
+  return (
+    <>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input id={id} className="outline-none" ref={ref} {...rest} />
+    </>
+  );
+});
+
+export default Input;
