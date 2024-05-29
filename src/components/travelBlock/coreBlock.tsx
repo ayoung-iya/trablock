fe6b67736469b19373acb735c55211e6db421f79
jinhok96 <jinhok96a@gmail.com>
2024-05-29 16:48:00 +0900

    chore: button 속성 추가 및 스타일 수정

--- src/components/travelBlock/coreBlock.tsx
+++ src/components/travelBlock/coreBlock.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-interface CoreBlockProps {
+interface CoreBlockProps extends React.HTMLAttributes<HTMLButtonElement> {
   name: string;
   tag: string;
   route?: { start: string; end: string };
@@ -8,12 +8,16 @@ interface CoreBlockProps {
   children?: React.ReactNode;
 }
 
-export default function CoreBlock({ name, tag, route, memo, children }: CoreBlockProps) {
+export default function CoreBlock({ name, tag, route, memo, children, onClick }: CoreBlockProps) {
   return (
-    <div className="flex flex-row items-center justify-between border border-solid border-black p-4">
-      <div>
-        <div>{name}</div>
-        <div className="flex flex-row">
+    <button
+      className="flex w-full items-center justify-between border border-solid border-black p-4"
+      type="button"
+      onClick={onClick}
+    >
+      <div className="flex flex-col gap-2">
+        <p>{name}</p>
+        <div className="flex gap-2">
           {/* 태그 컴포넌트 여기에 추가 */}
           <div>{tag}</div>
           {route && (
@@ -25,6 +29,6 @@ export default function CoreBlock({ name, tag, route, memo, children }: CoreBloc
         {memo && <p>{memo}</p>}
       </div>
       {children}
-    </div>
+    </button>
   );
 }
