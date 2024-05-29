e3f87583e4e0710ce7a9ba65ce5893ee9a90ab12
jinhok96 <jinhok96a@gmail.com>
2024-05-29 16:08:34 +0900

    chore: className 축

--- src/components/travelBlock/coreBlock.tsx
+++ src/components/travelBlock/coreBlock.tsx
@@ -10,16 +10,20 @@ interface CoreBlockProps {
 
 export default function CoreBlock({ name, tag, route, memo, children }: CoreBlockProps) {
   return (
-    <div>
-      <p>{name}</p>
-      {/* 태그 컴포넌트 여기에 추가 */}
-      <div>{tag}</div>
-      {route && (
-        <p>
-          {route.start} - {route.end}
-        </p>
-      )}
-      {memo && <p>{memo}</p>}
+    <div className="flex flex-row items-center justify-between border border-solid border-black">
+      <div>
+        <div>{name}</div>
+        {/* 태그 컴포넌트 여기에 추가 */}
+        <div className="flex flex-row">
+          <div>{tag}</div>
+          {route && (
+            <div>
+              {route.start} - {route.end}
+            </div>
+          )}
+        </div>
+        {memo && <p>{memo}</p>}
+      </div>
       {children}
     </div>
   );
