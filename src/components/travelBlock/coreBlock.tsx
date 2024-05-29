1be6b2a9add4c35d8d22d72716e7b859a5a994a2
jinhok96 <jinhok96a@gmail.com>
2024-05-29 16:12:38 +0900

    chore: 스타일 및 주석 수정

--- src/components/travelBlock/coreBlock.tsx
+++ src/components/travelBlock/coreBlock.tsx
@@ -10,11 +10,11 @@ interface CoreBlockProps {
 
 export default function CoreBlock({ name, tag, route, memo, children }: CoreBlockProps) {
   return (
-    <div className="flex flex-row items-center justify-between border border-solid border-black">
+    <div className="flex flex-row items-center justify-between border border-solid border-black p-4">
       <div>
         <div>{name}</div>
-        {/* 태그 컴포넌트 여기에 추가 */}
         <div className="flex flex-row">
+          {/* 태그 컴포넌트 여기에 추가 */}
           <div>{tag}</div>
           {route && (
             <div>
