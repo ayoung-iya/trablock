fa3934f84110d529d925a1c106ed5c9ce26cc683
jinhok96 <jinhok96a@gmail.com>
2024-05-29 17:58:49 +0900

    chore: 예시 수정

--- src/app/jinho/page.tsx
+++ src/app/jinho/page.tsx
@@ -4,7 +4,7 @@ import CoreBlock from '@/components/travelBlock/CoreBlock';
 
 export default function Jinho() {
   return (
-    <CoreBlock name="블록 이름" tag="태그" route={{ start: '출발지', end: '도착지' }} memo="메모입니다.">
+    <CoreBlock name="CoreBlock" tag="태그" route={{ start: '출발지', end: '도착지' }} memo="메모입니다.">
       <div>추가 요소</div>
     </CoreBlock>
   );
