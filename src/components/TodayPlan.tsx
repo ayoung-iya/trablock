286dab417e74d84a4d4dc3d571b078c20b56020f
kong33 <kge021114@naver.com>
2024-05-30 15:35:41 +0900

    fea: add Link on TodayPlan_16-17

--- src/components/TodayPlan.tsx
+++ src/components/TodayPlan.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
 
 type TodayPlanProps = {
   ImageUrl: StaticImageData;
@@ -8,15 +9,17 @@ type TodayPlanProps = {
 };
 export default function TodayPlan({ ImageUrl, TravelTitle, TravelDate, TravelSite }: TodayPlanProps) {
   return (
-    <div className="left-5f lex align-center translate-x-2/4transform fixed bottom-0 left-2/4 h-1/4 w-1/4 -translate-x-1/2 justify-center bg-teal-300">
-      <div className="relative h-1/2 w-1/4 rounded-full">
-        <Image src={ImageUrl} fill alt="TravelImage" />
+    <Link href="/">
+      <div className="left-5f lex align-center translate-x-2/4transform fixed bottom-0 left-2/4 h-1/4 w-1/4 -translate-x-1/2 justify-center bg-teal-300">
+        <div className="relative h-1/2 w-1/4 rounded-full">
+          <Image src={ImageUrl} fill alt="TravelImage" />
+        </div>
+        <div>
+          <h1>{TravelTitle}</h1>
+          <p>{TravelDate}</p>
+          <p>{TravelSite}</p>
+        </div>
       </div>
-      <div>
-        <h1>{TravelTitle}</h1>
-        <p>{TravelDate}</p>
-        <p>{TravelSite}</p>
-      </div>
-    </div>
+    </Link>
   );
 }
