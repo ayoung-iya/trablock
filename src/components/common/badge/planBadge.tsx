9b0faf69bc2211286e899dd85c1ac1f82a5d90ae
HaeJungg <gpwnd0120@gmail.com>
2024-05-30 16:52:12 +0900

    fix: 여행계획 플랜 뱃지 상태관관리 위치, props 변경

--- src/components/common/badge/planBadge.tsx
+++ src/components/common/badge/planBadge.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 interface PlanBadgeProps {
   text: string;
+  isSelected: boolean;
+  onClick: () => void;
 }
 
-export default function PlanBadge({ text }: PlanBadgeProps) {
-  return <div className="m-1 inline-flex items-center rounded-md bg-gray-200 px-4 py-2">{text}</div>;
+export default function PlanBadge({ text, isSelected, onClick }: PlanBadgeProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`m-1 inline-flex cursor-pointer items-center rounded-md px-4 py-2 focus:outline-none ${
+        isSelected ? 'bg-gray-700 text-white' : 'bg-gray-200 text-gray-700'
+      }`}
+    >
+      {text}
+    </button>
+  );
 }
