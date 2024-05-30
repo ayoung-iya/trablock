2521d345d69e9a8b7574f55ba7d213b635f206f6
kong33 <kge021114@naver.com>
2024-05-30 15:32:00 +0900

    feat: Create todayplan component and add example in gaeun page_16-17

---
+++ src/components/TodayPlan.tsx
@@ -0,0 +1,22 @@
+import Image, { StaticImageData } from 'next/image';
+
+type TodayPlanProps = {
+  ImageUrl: StaticImageData;
+  TravelTitle: string;
+  TravelDate: string;
+  TravelSite: string;
+};
+export default function TodayPlan({ ImageUrl, TravelTitle, TravelDate, TravelSite }: TodayPlanProps) {
+  return (
+    <div className="left-5f lex align-center translate-x-2/4transform fixed bottom-0 left-2/4 h-1/4 w-1/4 -translate-x-1/2 justify-center bg-teal-300">
+      <div className="relative h-1/2 w-1/4 rounded-full">
+        <Image src={ImageUrl} fill alt="TravelImage" />
+      </div>
+      <div>
+        <h1>{TravelTitle}</h1>
+        <p>{TravelDate}</p>
+        <p>{TravelSite}</p>
+      </div>
+    </div>
+  );
+}
