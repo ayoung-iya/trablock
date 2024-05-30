ea832ee3c8824cd41e65e7af681884da55e23f72
ayoung-iya <rhaehfdl1879@naver.com>
2024-05-31 01:15:36 +0900

    refact: 체크 여부 prop 제거 & onChange 추가

--- src/components/common/input/Checkbox.tsx
+++ src/components/common/input/Checkbox.tsx
@@ -1,18 +1,18 @@
 'use client';
 
-import { PropsWithChildren } from 'react';
+import React from 'react';
 
-interface CheckboxProps extends PropsWithChildren {
+interface CheckboxProps extends React.PropsWithChildren {
   value: string;
-  checked: boolean;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 // Checkbox 네이티브 대신 들어갈 내용을 children으로 전달한다
-export default function Checkbox({ checked, value, children }: CheckboxProps) {
+export default function Checkbox({ value, onChange, children }: CheckboxProps) {
   return (
-    <div className="relative min-w-48">
+    <div className="relative">
       {children}
-      <input type="checkbox" id={value} className="absolute inset-0 opacity-0" checked={checked} value={value} />
+      <input type="checkbox" className="absolute inset-0 opacity-0" value={value} onChange={onChange} />
     </div>
   );
 }
