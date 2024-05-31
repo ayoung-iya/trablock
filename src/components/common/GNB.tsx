ff7d441f804ee79dc979d198bf4a8a516eaa7ef8
kong33 <kge021114@naver.com>
2024-05-31 16:39:06 +0900

    feat: gnb 제작

--- src/components/common/GNB.tsx
+++ src/components/common/GNB.tsx
@@ -1,11 +1,15 @@
+import { cookies } from 'next/headers';
+import Image from 'next/image';
+
 import mockLogo from '@/icons/googleLogo.png';
 import mockSearchIcon from '@/icons/mockSearchLogo.png';
 
-import Image from 'next/image';
-
 export default function GNB() {
-  //프로필은 로그인 상태일 때만 나타내기
-  //계획하기 페이지는 로그인 상태가 아니면 로그인 하기 페이지로 ㄱㄱ
+  const cookieStore = cookies();
+  const hasCookie = cookieStore.has('user');
+  // 추후 쿠키 저장 이름에 따라 달라질 예정
+  // 계획하기 페이지는 로그인 상태가 아니면 로그인 하기 페이지 이동
+
   return (
     <nav className="flex w-full space-x-7 bg-slate-400">
       <Image src={mockLogo} alt="logo" width="100" height="50" />
@@ -16,7 +20,7 @@ export default function GNB() {
         <li>
           <button type="button">계획하기</button>
         </li>
-        <li>프로필 컴포넌트</li>
+        {hasCookie && <li>프로필 사진 컴포넌트</li>}
       </ul>
     </nav>
   );
