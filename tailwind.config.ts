701a32466750cf57ec7f4725e7183158833a10a6
kong33 <kge021114@naver.com>
2024-05-31 17:46:59 +0900

    fix: tailwind config에 추가한 extend 삭제_T6-17

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,11 +11,6 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      },
-      extend: {
-        translate: {
-          '-1/2': '-50%'
-        }
       }
     }
   },
