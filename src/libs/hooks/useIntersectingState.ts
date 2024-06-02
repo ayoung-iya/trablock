959f6a6c101b10a0e8dc846392ea1f0160072302
kong33 <kge021114@naver.com>
2024-06-02 15:44:46 +0900

    Feat: useIntersectingState 훅 구현_T6-64

---
+++ src/libs/hooks/useIntersectingState.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-redeclare */
+import { useEffect, useRef, useState } from 'react';
+import type { RefObject } from 'react';
+
+function useIntersectingState<T extends Element>(): [boolean, RefObject<T>];
+
+function useIntersectingState<T extends Element>(initialState: null): [boolean | null, RefObject<T>];
+
+function useIntersectingState<T extends Element>(initialState?: null): [boolean | (boolean | null), RefObject<T>] {
+  const [isIntersecting, setIsIntersecting] = useState(initialState === null ? null : false);
+  const ref = useRef<T>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return [isIntersecting, ref];
+}
+
+export default useIntersectingState;
