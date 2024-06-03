97a27f87415273fedff41adf5b8e90716967b9ba
HaeJungg <gpwnd0120@gmail.com>
2024-06-03 14:43:10 +0900

    fix: 상수파일 추가, props 수정

--- src/components/common/badge/blockBadge.tsx
+++ src/components/common/badge/blockBadge.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
+import CATEGORY from '@/libs/constants/BlockCategories';
+
 interface BlockBadgeProps {
-  text: string;
-  bgColor: string;
-  textColor: string;
-  borderColor: string;
+  variant: 'spot' | 'restaurant' | 'traffic' | 'activity' | 'accommodation' | 'custom';
 }
 
-// 디자인이 어떻게 나올지 모르겠는데 우선 props로 해당 값을 받을 수 있도록 만들었습니다.
+export default function BlockBadge({ variant }: BlockBadgeProps) {
+  const { name, color } = CATEGORY[variant];
 
-export default function BlockBadge({ text, bgColor, textColor, borderColor }: BlockBadgeProps) {
-  return (
-    <div className={`m-1 inline-flex items-center px-3 py-1 ${bgColor} ${textColor} ${borderColor} border-2`}>
-      {text}
-    </div>
-  );
+  return <div className={`m-1 inline-flex items-center px-3 py-1 ${color} border-2`}>{name}</div>;
 }
