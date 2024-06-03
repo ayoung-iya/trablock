29c2b727953505907d692181619d87a643be17aa
jinhok96 <jinhok96a@gmail.com>
2024-06-03 16:00:05 +0900

    chore: TravelCard 추가, 두 가지 커스텀 훅 추가

--- src/components/card/TravelCard.tsx
+++ src/components/card/TravelCard.tsx
@@ -2,11 +2,13 @@
 
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 
 import Image from 'next/image';
 
 import { DefaultCardProps } from '@/components/card/type';
+import useMediaQuery from '@/libs/hooks/useMediaQueryMinWidth';
+import useResizeHeight from '@/libs/hooks/useResizeHeight';
 
 interface TravelCardProps extends DefaultCardProps {
   route: string[];
@@ -28,82 +30,69 @@ export default function TravelCard({
   comment,
   onClick
 }: TravelCardProps) {
-  // 콘텐츠 높이에 따라 이미지 높이를 동적으로 조절하기 위한 코드
-  const contentRef = useRef<HTMLDivElement>(null);
-  const [contentHeight, setContentHeight] = useState(0);
-  const [isMd, setIsMd] = useState(false);
+  const { divRef, divHeight } = useResizeHeight();
+  const isMd = useMediaQuery('(min-width: 768px)');
 
-  useEffect(() => {
-    const updateHeight = () => {
-      if (contentRef.current && contentRef.current.clientHeight !== contentHeight) {
-        setContentHeight(contentRef.current.clientHeight);
-      }
-    };
-
-    const handleResize = () => {
-      if (window.matchMedia('(min-width: 768px)').matches) {
-        setIsMd(true);
-      } else {
-        setIsMd(false);
-      }
-      updateHeight();
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [contentHeight]);
+  const buttonStyle = isMd ? { maxHeight: `${divHeight}px` } : undefined;
 
   return (
-    <button
-      className="md:flex-row-center relative flex size-full overflow-hidden border border-solid max-md:flex-col"
-      style={isMd ? { maxHeight: `${contentHeight}px` } : undefined}
-      type="button"
-      onClick={onClick}
-    >
-      {/* 대표 이미지 */}
-      <div className="flex-row-center w-full overflow-hidden max-md:max-h-52 md:w-auto md:max-w-64">
-        <Image src={imageUrl} alt={imageUrl} width={80 * 4} height={80 * 4} className="image-cover" />
-      </div>
-      {/* 콘텐츠 */}
-      <div className="flex-grow max-md:w-full" ref={contentRef}>
-        {/* 상단 각종 데이터 */}
-        <div className="flex flex-col gap-2 border-b p-4">
-          <p>{title}</p>
-          <div>
-            {route.map((item, idx) => (
-              <span key={item}>
-                <span>{item}</span>
-                {idx < route.length - 1 && <span> → </span>}
-              </span>
-            ))}
-            <p>
-              {period.start} ~ {period.end}
-            </p>
-          </div>
-          <div className="flex-row-center flex-wrap gap-2">
-            {tag.map((item) => (
-              <p key={item} className="flex-shrink-0 bg-slate-300">
-                #{item}
+    <>
+      {/* css에서 동적인 수치는 반영되지 않으므로 style에 직접 주입 */}
+      <button
+        className="md:flex-row-center relative flex size-full overflow-hidden border border-solid max-md:flex-col"
+        style={buttonStyle}
+        type="button"
+        onClick={onClick}
+      >
+        {/* 대표 이미지 */}
+        <div className="flex-row-center w-full overflow-hidden max-md:max-h-52 md:w-auto md:max-w-64">
+          <Image src={imageUrl} alt={imageUrl} width={80 * 4} height={80 * 4} className="image-cover" />
+        </div>
+        {/* 콘텐츠 */}
+        <div className="flex-grow max-md:w-full" ref={divRef}>
+          {/* 상단 각종 데이터 */}
+          <div className="flex flex-col gap-2 border-b p-4">
+            <p>{title}</p>
+            <div>
+              {route.map((item, idx) => (
+                <span key={item}>
+                  <span>{item}</span>
+                  {idx < route.length - 1 && <span> → </span>}
+                </span>
+              ))}
+              <p>
+                {period.start} ~ {period.end}
               </p>
-            ))}
-          </div>
-        </div>
-        {/* 하단 프로필, 카운트 */}
-        <div className="flex-row-center justify-between p-3">
-          <div className="flex-row-center gap-2">
-            <div className="size-full max-h-8 max-w-8 overflow-hidden rounded-full">
-              <Image src={user.profileImg} alt={user.profileImg} width={8 * 4} height={8 * 4} className="image-cover" />
             </div>
-            <p>{user.name}</p>
+            <div className="flex-row-center flex-wrap gap-2">
+              {tag.map((item) => (
+                <p key={item} className="flex-shrink-0 bg-slate-300">
+                  #{item}
+                </p>
+              ))}
+            </div>
           </div>
-          <div className="flex-row-center gap-2">
-            <div>❤️{favorite}</div>
-            <div>🌈{comment}</div>
+          {/* 하단 프로필, 카운트 */}
+          <div className="flex-row-center justify-between p-3">
+            <div className="flex-row-center gap-2">
+              <div className="size-full max-h-8 max-w-8 overflow-hidden rounded-full">
+                <Image
+                  src={user.profileImg}
+                  alt={user.profileImg}
+                  width={8 * 4}
+                  height={8 * 4}
+                  className="image-cover"
+                />
+              </div>
+              <p>{user.name}</p>
+            </div>
+            <div className="flex-row-center gap-2">
+              <div>❤️{favorite}</div>
+              <div>🌈{comment}</div>
+            </div>
           </div>
         </div>
-      </div>
-    </button>
+      </button>
+    </>
   );
 }
