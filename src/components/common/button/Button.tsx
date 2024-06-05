f0750580a4bb943cdc779a1d17bc5e3c5af4bb0e
HaeJungg <gpwnd0120@gmail.com>
2024-06-05 20:59:40 +0900

    fix: props 수정

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -39,7 +39,7 @@ interface ButtonProps {
   size?: 'sm' | 'md' | 'lg';
   children: React.ReactNode;
   className?: string;
-  onClick?: () => void;
+  onClick: () => void;
   type?: 'button' | 'submit' | 'reset';
 }
 
