7d3bc8b090dc3250167150e41584e779a7fafea9
HaeJungg <gpwnd0120@gmail.com>
2024-06-05 14:03:12 +0900

    fix: 기본 버튼 수정

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 
 interface ButtonProps {
   onClick: () => void;
-  variant?: 'solid' | 'light' | 'ghost' | 'outline' | 'gary' | 'red';
-  size?: 'sm' | 'lg';
-  disabled?: boolean;
+  variant?: 'solid' | 'light' | 'ghost' | 'outline' | 'gray' | 'red';
+  size?: 'sm' | 'md' | 'lg';
   children: React.ReactNode;
 }
 
-export default function Button({ children, onClick, variant = 'solid', size = 'lg', disabled = false }: ButtonProps) {
+// 변형 스타일
+const variantStyles = {
+  solid: 'bg-primary-01 text-white-01',
+  light: 'bg-primary-03 text-primary-01',
+  ghost: 'border border-primary-01 bg-white-01 text-primary-01',
+  outline: 'border border-gray-02 text-black-02',
+  gray: 'bg-gray-02 text-white-01 cursor-not-allowed',
+  red: 'bg-red-01 text-white-01'
+};
+
+// 크기 스타일
+const sizeStyles = {
+  sm: 'px-[24px] py-2 max-h-[56px]',
+  md: 'max-w-[320px] max-h-[48px]',
+  lg: 'max-w-[346px] max-h-[40px]'
+};
+
+export default function Button({ children, onClick, variant = 'solid', size = 'lg' }: ButtonProps) {
   // 기본스타일
   const baseStyles = 'flex justify-center items-center items-center gap-[10px] rounded-[5px]';
-
   return (
-    <button
-      type="button"
-      onClick={onClick}
-      className={`${baseStyles} btn-${variant} btn-${size} ${disabled ? 'disabled' : ''}`}
-      disabled={disabled}
-    >
+    <button type="button" onClick={onClick} className={`${baseStyles} ${variantStyles[variant]} ${sizeStyles[size]}`}>
       {children}
     </button>
   );
