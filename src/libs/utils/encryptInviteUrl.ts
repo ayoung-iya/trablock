092dc140bc0b559d1da831076af4cb57c9303fc2
kong33 <kge021114@naver.com>
2024-06-07 02:00:17 +0900

    Fix : encriptinviteUrl 수정

--- src/libs/utils/encryptInviteUrl.ts
+++ src/libs/utils/encryptInviteUrl.ts
@@ -2,7 +2,12 @@ import crypto from 'crypto';
 
 export default function encryptInviteUrl(date: Date, planId: string) {
   const data = date + planId;
-  const dataEncode = crypto.createHash('sha256').update(data).digest('base64url');
+  const secretKey = crypto.randomBytes(16);
+  const iv = crypto.randomBytes(16);
 
-  return dataEncode;
+  const cipher = crypto.createCipheriv('aes-128-cbc', secretKey, iv);
+  let encrypted = cipher.update(data, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+
+  return { iv: iv.toString('hex'), content: encrypted };
 }
