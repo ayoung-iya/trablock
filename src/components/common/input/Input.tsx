aa4ecb662a52b17c9dbe8effff88c62018d513e3
ayoung-iya <rhaehfdl1879@naver.com>
2024-06-07 02:09:55 +0900

    refact: 코어 인풋의 스타일은 prop으로 넘겨주는 것으로 변경

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -2,40 +2,13 @@ import React, { forwardRef } from 'react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
-  isError?: boolean;
-  variant: 'sign' | 'search';
 }
 
-const INPUT_STYLE = {
-  sign: {
-    default: 'bg-white-01 w-full h-12 px-3 pb-3 pt-[14px] rounded-[5px] border border-gray-02',
-    focus: 'focus:border-primary-01',
-    error: 'border-red-01 focus:border-red-01',
-    text: 'font-body-2 text-black-01'
-  },
-  search: {
-    default: 'bg-inherit w-full',
-    focus: '',
-    error: '',
-    text: 'font-caption-1 text-black-02'
-  }
-};
-
 const Input = forwardRef(function Input(
-  { id, variant, isError, ...rest }: InputProps,
+  { id, className, ...rest }: InputProps,
   ref: React.ForwardedRef<HTMLInputElement>
 ) {
-  const inputStyle = INPUT_STYLE[variant];
-
-  return (
-    <input
-      id={id}
-      // eslint-disable-next-line max-len
-      className={`peer outline-none ${inputStyle.default} ${inputStyle.focus} ${(isError && inputStyle.error) || ''} ${inputStyle.text}`}
-      ref={ref}
-      {...rest}
-    />
-  );
+  return <input id={id} className={`outline-none ${className}`} ref={ref} {...rest} />;
 });
 
 export default Input;
--- src/components/common/input/SignInput.tsx
+++ src/components/common/input/SignInput.tsx
@@ -2,6 +2,15 @@ import React, { forwardRef } from 'react';
 
 import Input from './Input';
 
+const INPUT_STYLE = {
+  default: 'bg-white-01 w-full h-12 px-3 pb-3 pt-[14px] rounded-[5px] border border-gray-02',
+  focus: 'focus:border-primary-01',
+  error: 'border-red-01 focus:border-red-01',
+  text: 'font-body-2 text-black-01'
+};
+
+const LABEL_ERROR_STYLE = 'text-red-01 peer-focus-within:text-red-01';
+
 interface SignInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
   label?: string;
@@ -13,15 +22,19 @@ const SignInput = forwardRef(function SignInput(
   ref: React.ForwardedRef<HTMLInputElement>
 ) {
   const isError = !!errorMessage;
-  const labelErrorStyle = 'text-red-01 peer-focus-within:text-red-01';
 
   return (
     <>
       <div className="flex flex-col-reverse gap-1">
-        <Input id={id} variant="sign" ref={ref} {...rest} isError={isError} />
+        <Input
+          className={`${INPUT_STYLE.default} ${INPUT_STYLE.text} ${INPUT_STYLE.focus} ${isError && INPUT_STYLE.error}`}
+          id={id}
+          ref={ref}
+          {...rest}
+        />
         <label
           htmlFor={id}
-          className={`font-subtitle-3 text-gray-01 peer-focus-within:text-primary-01 ${isError && labelErrorStyle}`}
+          className={`font-subtitle-3 text-gray-01 peer-focus-within:text-primary-01 ${isError && LABEL_ERROR_STYLE}`}
         >
           {label}
         </label>
