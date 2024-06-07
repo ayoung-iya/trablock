34e89bb5bd6b6dc651471704dc1dbc932f371978
ayoung-iya <rhaehfdl1879@naver.com>
2024-06-08 04:02:13 +0900

    design: 드랍다운 기본 스타일 적용

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -3,7 +3,11 @@ import React, { forwardRef } from 'react';
 interface DropdownProps extends React.PropsWithChildren {}
 
 const Dropdown = forwardRef(function Dropdown({ children }: DropdownProps, ref: React.ForwardedRef<HTMLUListElement>) {
-  return <ul ref={ref}>{children}</ul>;
+  return (
+    <ul className="bg-white-01 rounded-[10px] p-5 shadow-[0_0_10px_0_rgba(0,0,0,0.1)]" ref={ref}>
+      {children}
+    </ul>
+  );
 });
 
 export default Dropdown;
