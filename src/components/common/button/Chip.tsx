8d56fc0e50d52b40ff94d8cceadfbf244046ccad
HaeJungg <gpwnd0120@gmail.com>
2024-06-10 13:30:10 +0900

    fix: 컴포넌트 props 네이밍 변경

--- src/components/common/button/Chip.tsx
+++ src/components/common/button/Chip.tsx
@@ -6,7 +6,7 @@ import classNames from 'classnames';
  * chip 컴포넌트
  * @component
  *
- * @param {('lg' | 'sm')} [size='lg'] - 칩의 크기
+ * @param {('day' | 'default')} [variant='default'] - 칩의 변형
  * @param {boolean} selected - 칩의 선택 상태
  *
  * const [isSelectedLarge, setIsSelectedLarge] = useState(false);
@@ -17,32 +17,32 @@ import classNames from 'classnames';
 
 const chipStyles: { [key: string]: string } = {
   base: 'flex px-5 py-1.75 justify-center items-center gap-2.5 rounded-full font-caption-1',
-  selectedLg: 'bg-black-01 text-white-01',
-  selectedSm: 'border-solid border-secondary-01 bg-white text-secondary-01',
-  defaultLg: 'border-solid border-gray-02 bg-white-01 text-black-02',
-  defaultSm: 'border-solid border-gray-02 bg-white-01 text-black-02',
-  lg: 'h-10',
-  sm: 'h-9'
+  selectedDay: 'bg-black-01 text-white-01',
+  selectedDefault: 'border-solid border-secondary-01 bg-white text-secondary-01',
+  unselectedDay: 'border-solid border-gray-02 bg-white-01 text-black-02',
+  unselectedDefault: 'border-solid border-gray-02 bg-white-01 text-black-02',
+  day: 'h-10',
+  default: 'h-9'
 };
 
 interface ChipProps {
   children: React.ReactNode;
-  size?: 'lg' | 'sm';
+  variant?: 'day' | 'default';
   selected: boolean;
   onClick: () => void;
 }
 
-export default function Chip({ children, size = 'lg', selected, onClick }: ChipProps) {
-  const sizeClass = size === 'lg' ? chipStyles.lg : chipStyles.sm;
+export default function Chip({ children, variant = 'default', selected, onClick }: ChipProps) {
+  const variantClass = variant === 'day' ? chipStyles.day : chipStyles.default;
   const stateClass = selected
-    ? size === 'lg'
-      ? chipStyles.selectedLg
-      : chipStyles.selectedSm
-    : size === 'lg'
-      ? chipStyles.defaultLg
-      : chipStyles.defaultSm;
+    ? variant === 'day'
+      ? chipStyles.selectedDay
+      : chipStyles.selectedDefault
+    : variant === 'day'
+      ? chipStyles.unselectedDay
+      : chipStyles.unselectedDefault;
 
-  const chipClass = classNames(chipStyles.base, sizeClass, stateClass);
+  const chipClass = classNames(chipStyles.base, variantClass, stateClass);
 
   return (
     <button type="button" className={chipClass} onClick={onClick}>
