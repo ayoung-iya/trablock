6c4c0db88a5c417d6a6353fcc4f535315d0cfb7f
ayoung-iya <rhaehfdl1879@naver.com>
2024-06-11 14:59:37 +0900

    refact: 검색어 빈문자열로 기본값 설정 & 이벤트핸들러가 있으면 동작

--- src/components/common/input/SearchInput.tsx
+++ src/components/common/input/SearchInput.tsx
@@ -4,9 +4,10 @@ import Input from './Input';
 
 interface SearchInputParams {
   searchString?: string;
+  onClickSearchIcon?: (searchString: string) => void;
 }
 
-export default function SearchInput({ searchString: initialSearchString }: SearchInputParams) {
+export default function SearchInput({ searchString: initialSearchString = '', onClickSearchIcon }: SearchInputParams) {
   const [searchString, setSearchString] = useState(initialSearchString);
 
   const handleSearchStringChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
@@ -17,6 +18,12 @@ export default function SearchInput({ searchString: initialSearchString }: Searc
     setSearchString('');
   };
 
+  const handleSearchIconClick = () => {
+    if (onClickSearchIcon) {
+      onClickSearchIcon(searchString);
+    }
+  };
+
   return (
     <div className="bg-gray-03 flex h-10 rounded-[5px] px-3 pb-2 pt-[10px]">
       <Input id="search" value={searchString} className="w-full bg-inherit" onChange={handleSearchStringChange} />
@@ -25,7 +32,9 @@ export default function SearchInput({ searchString: initialSearchString }: Searc
           X
         </button>
       )}
-      <span className="size-5 flex-shrink-0">O</span>
+      <button type="submit" className="size-5 flex-shrink-0" onClick={handleSearchIconClick}>
+        O
+      </button>
     </div>
   );
 }
