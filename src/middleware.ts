c7fd6ce22b37f94e8d680f097455d5ec35fbd177
kong33 <kge021114@naver.com>
2024-06-12 13:09:47 +0900

    Fix : middleware 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
-  const { pathname } = request.nextUrl;
-  const cookie = request.cookies.get('authToken'); // 백엔드와 상의 후 이름 변경
-  const targetPage = url.searchParams.get('returnUrl') || '/';
-  const isInvitePath = url.pathname.startsWith('/plan/'); // invite 페이지는 plan으로 시작함.
+  const url = request.nextUrl.pathname;
 
-  if (!cookie && isInvitePath) {
-    // 초대 받았는데 비로그인 상태일때 !
-    url.pathname = '/login';
-    url.search = `?returnUrl=${encodeURIComponent(targetPage)}`;
-    return NextResponse.redirect(url);
-  }
-
-  if (pathname === '/') {
+  if (url === '/') {
     const response = NextResponse.next();
     response.headers.set('X-Skip-Icons', 'true');
     return response;
@@ -23,5 +12,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/']
+  matcher: ['/']
 };
