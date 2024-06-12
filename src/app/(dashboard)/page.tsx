808b6ca866c727730b5e2ba5e2290537d583789a
HaeJungg <gpwnd0120@gmail.com>
2024-06-12 13:20:29 +0900

    chore: page 되돌리기

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,22 +1,5 @@
 /* eslint-disable max-len */
 
-'use client';
-
-import React from 'react';
-
-import ArrowButton from '@/components/common/button/Arrow';
-
 export default function Home() {
-  const handleClick = () => {
-    console.log('Arrow button clicked');
-  };
-
-  return (
-    <div>
-      <ArrowButton direction="left" onClick={handleClick} />
-      <ArrowButton direction="left" onClick={handleClick} disabled />
-      <ArrowButton direction="right" onClick={handleClick} />
-      <ArrowButton direction="right" onClick={handleClick} disabled />
-    </div>
-  );
+  return <div>Page</div>;
 }
