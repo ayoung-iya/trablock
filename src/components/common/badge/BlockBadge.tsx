1942795d436a9730e77d64baab638f78d56dcc98
HaeJungg <gpwnd0120@gmail.com>
2024-06-14 19:05:17 +0900

    fix: 상수 파일 안으로 이동, type이 ㅈ지정되지 않는 경우 추가

--- src/components/common/badge/BlockBadge.tsx
+++ src/components/common/badge/BlockBadge.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-import CATEGORY from '@/libs/constants/BlockCategories';
+// 블럭 카테고리 상수
+const CATEGORY = {
+  spot: { name: '관광지', color: 'bg-blue-200 text-black border-blue-500' },
+  restaurant: { name: '식당', color: 'bg-red-200 text-black border-red-500' },
+  traffic: { name: '교통', color: 'bg-yellow-200 text-black border-yellow-500' },
+  activity: { name: '엑티비티', color: 'bg-purple-500 text-white border-purple-700' },
+  accommodation: { name: '숙소', color: 'bg-green-200 text-black border-green-500' },
+  custom: { name: '기타', color: 'bg-gray-200 text-black border-gray-500' }
+};
 
 interface BlockBadgeProps {
-  variant: 'spot' | 'restaurant' | 'traffic' | 'activity' | 'accommodation' | 'custom';
+  type?: 'spot' | 'restaurant' | 'traffic' | 'activity' | 'accommodation' | 'custom';
+  className?: string;
 }
 
-export default function BlockBadge({ variant }: BlockBadgeProps) {
-  const { name, color } = CATEGORY[variant];
+export default function BlockBadge({ type, className }: BlockBadgeProps) {
+  const baseStyle = ' ';
+  const { name, color } = type ? CATEGORY[type] : { name: '', color: '' };
+  const finalClassName = type ? `${baseStyle} ${color}` : `${baseStyle} ${className}`;
 
-  return <div className={`m-1 inline-flex items-center px-3 py-1 ${color} border-2`}>{name}</div>;
+  return <div className={finalClassName}>{name}</div>;
 }
