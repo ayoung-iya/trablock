ed1267fa5a22d8ed89f2de8b7d27d43b272aaf64
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-06-17 23:35:47 +0900

    T6-99 Add custom types

--- src/app/(write)/writeTest/page.tsx
+++ src/app/(write)/writeTest/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import SlateEditor from '@/components/textEditor/SlateEditor';
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col gap-10">
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <SlateEditor />
+    </div>
+  );
 };
 
 export default page;
