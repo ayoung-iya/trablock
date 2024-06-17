79e69f0cea32cb4decd5b5e84ada6334bad97468
gaeun <kge0211114@gmail.com>
2024-06-18 01:59:38 +0900

    Feat: 회원가입 완성

--- src/apis/useSignup/fetch.ts
+++ src/apis/useSignup/fetch.ts
@@ -1,19 +1,38 @@
+/*eslint-disable*/
+
 import returnFetch, { ReturnFetchDefaultOptions } from 'return-fetch';
 
 import { signupProps } from '@/components/SignupForm';
 
 const options: { [key: string]: ReturnFetchDefaultOptions } = {
   signup: {
-    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
-    headers: { 'Content-Type': 'application/json' }
+    //baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    baseUrl: 'https://be.travel-laboratory.site',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  },
+  checkUsername: {
+    baseUrl: 'https://be.travel-laboratory.site',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  },
+  checkNickname: {
+    baseUrl: 'https://be.travel-laboratory.site',
+    headers: {
+      'Content-Type': 'application/json'
+    }
   }
 };
 
 const fetchSignup = returnFetch(options.signup);
+const fetchCheckUsername = returnFetch(options.checkUsername);
+const fetchCheckNickname = returnFetch(options.checkNickname);
 
 const serviceSignup = {
   postSignup: async (data: signupProps) => {
-    const response = await fetchSignup('api/v1/auth/join', {
+    const response = await fetchSignup('/api/v1/auth/join', {
       method: 'POST',
       body: JSON.stringify(data)
     });
@@ -21,7 +40,7 @@ const serviceSignup = {
     return result;
   },
   postUsernameCheck: async (data: string) => {
-    const response = await fetchSignup('api/v1/auth/username', {
+    const response = await fetchCheckUsername('/api/v1/auth/username', {
       method: 'POST',
       body: JSON.stringify(data)
     });
@@ -29,7 +48,7 @@ const serviceSignup = {
     return result;
   },
   postNicknameCheck: async (data: string) => {
-    const response = await fetchSignup('api/v1/auth/nickname', {
+    const response = await fetchCheckNickname('/api/v1/auth/nickname', {
       method: 'POST',
       body: JSON.stringify(data)
     });
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,105 +1,135 @@
-// /* eslint-disable */
+/* eslint-disable */
 
-// 'use client';
+'use client';
 
-// import { useForm, SubmitHandler, FieldValues } from 'react-hook-form';
+import { useForm, SubmitHandler, FieldValues } from 'react-hook-form';
 
-// import Checkbox from '@/components/common/input/Checkbox';
-// import SignInput from '@/components/common/input/SignInput';
-// import PlanInputTitle from '@/components/PlanInputTitle';
-// import { validate } from '@/libs/constants/validation';
-// import usePostSignup from '@/apis/useSignup/usePostSignup';
-// import usePostNicknameCheck from '@/apis/useSignup/usePostNicknameCheck;
+import Checkbox from '@/components/common/input/Checkbox';
+import SignInput from '@/components/common/input/SignInput';
+import PlanInputTitle from '@/components/PlanInputTitle';
+import { validate } from '@/libs/constants/validation';
+import usePostSignup from '@/apis/useSignup/usePostSignup';
+import usePostNicknameCheck from '@/apis/useSignup/usePostNicknameCheck';
+import usePostUsernameCheck from '@/apis/useSignup/usePostUsernamCheck';
+import { redirect } from 'next/navigation';
 
-// export interface signupProps {
-//   username: string;
-//   password: string;
-//   nickname: string;
-//   pw_question_id: number;
-//   pw_answer: string;
-//   is_agreement: boolean;
-// }
+export interface signupProps {
+  username: string;
+  password: string;
+  nickname: string;
+  pw_question_id: number;
+  pw_answer: string;
+  is_agreement: boolean;
+}
 
-// export default function SignupForm() {
-//   const {
-//     register,
-//     handleSubmit,
-//     setError,
-//     watch,
-//     formState: { errors, isValid }
-//   } = useForm();
+export default function SignupForm() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    watch,
+    formState: { errors, isValid }
+  } = useForm();
 
-//   const payload = {
-//     username: watch('nickname'),
-//     password: watch('username'),
-//     nickname: watch('password'),
-//     pw_question_id: watch('pw_question_id'),
-//     pw_answer: watch('pw_answer'),
-//     is_agreement: watch('is_agreement')
-//   };
+  const payload = {
+    username: watch('username'),
+    password: watch('password'),
+    nickname: watch('nickname'),
+    pw_question_id: watch('pw_question_id'),
+    pw_answer: watch('pw_answer'),
+    is_agreement: watch('is_agreement')
+  };
 
-//   const { mutate: postSignupMutate } = usePostSignup();
-// const { mutate: postNicknameCheckMutate} = usePostNicknameCheck({
-//   onSuccess:(data)=> {
-//     if()
-//   }
-// });
+  const passwordCheckWatch = watch('password_confirm');
+  const { mutate: postSignupMutate } = usePostSignup();
+  const { mutate: postNicknameCheckMutate } = usePostNicknameCheck();
+  const { mutate: postUsernameCheckMutate } = usePostUsernameCheck();
 
-//   const registerList = {
-//     username: register('username', { ...validate.username, onBlur: () => validateUsername() }),
-//     password: register('password', validate.password),
-//     passwordCheck: register('password_confirm', { onBlur: () => validatePasswordCheck() }),
-//     nickname: register('nickname', { ...validate.nickname, onBlur: () => validateNickname() }),
-//     pw_answer: register('pw_answer', validate.pw_answer)
-//   };
+  const validateUsername = () => {
+    //username fetch
+    postUsernameCheckMutate(payload.username, {
+      onSuccess: (response) => {
+        if (response.available) {
+          clearErrors('username');
+        } else {
+          setError('username', { message: '중복된 이메일입니다.' });
+        }
+      }
+    });
+  };
+  const validatePasswordCheck = () => {
+    if (payload.password !== passwordCheckWatch) {
+      setError('password_confirm', { type: 'password-mismatch', message: '비밀번호가 일치하지 않습니다.' });
+    } else {
+      clearErrors('password_confirm');
+    }
+  };
+  const validateNickname = () => {
+    //fetch해서 결과에 따라 seterror or not(409)
+    console.log(payload.nickname as string);
+    postNicknameCheckMutate(payload.nickname, {
+      onSuccess: (response) => {
+        if (response.available) {
+          clearErrors('nickname');
+        } else {
+          setError('nickname', { message: '중복된 닉네임입니다.' });
+        }
+      }
+    });
+  };
 
-//   const validateUsername = () => {
-//     //username fetch
-//     console.log('a')
-//   };
-//   const validatePasswordCheck = () => {
-//     //c
-//     console.log('e')
-//   };
-//   const validateNickname = () => {
-//     //fetch해서 결과에 따라 seterror or not(409)
-//     postNicknameCheckMutate(payload.nickname);
+  const registerList = {
+    username: register('username', { ...validate.username, onBlur: () => validateUsername() }),
+    password: register('password', validate.password),
+    passwordCheck: register('password_confirm', { onBlur: () => validatePasswordCheck() }),
+    nickname: register('nickname', { ...validate.nickname, onBlur: () => validateNickname() }),
+    pw_answer: register('pw_answer', validate.pw_answer)
+  };
 
-//   };
+  const onSubmit: SubmitHandler<FieldValues> = () => {
+    postSignupMutate(payload, {
+      onSuccess: (data) => {
+        console.log(data);
+        redirect('/');
+      }
+    });
+  };
 
-//   const onSubmit: SubmitHandler<FieldValues> = () => {
-//     postSignupMutate(payload);
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <PlanInputTitle>기본정보 입력</PlanInputTitle>
-//       <SignInput
-//         label="닉네임"
-//         id="nickname"
-//         errorMessage={errors.nickname?.message as string}
-//         {...registerList.nickname}
-//       />
-//       <SignInput
-//         label="이메일"
-//         id="username"
-//         errorMessage={errors.username?.message as string}
-//         {...registerList.username}
-//       />
-//       <SignInput
-//         label="비밀번호"
-//         id="password"
-//         errorMessage={errors.password?.message as string}
-//         {...registerList.password}
-//       />
-//       <SignInput label="비밀번호 확인" id="password_confirm" errorMessage="비번이랑 같지 않으면 에러" />
-//       <PlanInputTitle>비밀번호 찾기 정보 입력</PlanInputTitle>
-//       <SignInput label="질문" id="pw_question_id" />
-//       <PlanInputTitle>약관 동의</PlanInputTitle>
-//       <Checkbox id="is_agreement">개인정보 수집 및 이용 동의</Checkbox>
-//       <button disabled={isValid} type="submit">
-//         회원가입
-//       </button>
-//     </form>
-//   );
-// }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <PlanInputTitle>기본 정보 입력</PlanInputTitle>
+      <SignInput
+        label="닉네임"
+        id="nickname"
+        errorMessage={errors.nickname?.message as string}
+        {...registerList.nickname}
+      />
+      <SignInput
+        label="이메일"
+        id="username"
+        errorMessage={errors.username?.message as string}
+        {...registerList.username}
+      />
+      <SignInput
+        label="비밀번호"
+        id="password"
+        errorMessage={errors.password?.message as string}
+        {...registerList.password}
+      />
+      <SignInput
+        label="비밀번호 확인"
+        id="password_confirm"
+        errorMessage={errors.password_confirm?.message as string}
+      />
+      <PlanInputTitle>비밀번호 정보 입력</PlanInputTitle>
+      <SignInput label="질문" id="pw_question_id" />
+      <SignInput label="답변" id="pw_answer" />
+      <PlanInputTitle>약관 동의</PlanInputTitle>
+      <Checkbox id="is_agreement">개인정보 수집 및 이용 동의</Checkbox>
+      <button disabled={isValid} type="submit">
+        회원가입
+      </button>
+    </form>
+  );
+}
