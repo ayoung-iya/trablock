3e0d8391be9133fe17153c0326774e836756b7e5
jinhok96 <jinhok96a@gmail.com>
2024-06-19 14:36:38 +0900

    chore: useDropdown 주석 처리

--- src/libs/hooks/useDropdown.ts
+++ src/libs/hooks/useDropdown.ts
@@ -1,45 +1,45 @@
-import { useEffect, useRef, useState } from 'react';
+// import { useEffect, useRef, useState } from 'react';
 
-interface useDropdownParams {
-  onClickInside?: (e?: MouseEvent) => void;
-}
+// interface useDropdownParams {
+//   onClickInside?: (e?: MouseEvent) => void;
+// }
 
-const useDropdown = ({ onClickInside }: useDropdownParams) => {
-  const [isDropdownOpened, setIsDropdownOpened] = useState(false);
-  const ref = useRef<HTMLUListElement>(null);
+// const useDropdown = ({ onClickInside }: useDropdownParams) => {
+//   const [isDropdownOpened, setIsDropdownOpened] = useState(false);
+//   const ref = useRef<HTMLUListElement>(null);
 
-  const handleDropdownOpen = () => {
-    setIsDropdownOpened(true);
-  };
+//   const handleDropdownOpen = () => {
+//     setIsDropdownOpened(true);
+//   };
 
-  const handleDropdownClose = () => {
-    setIsDropdownOpened(false);
-  };
+//   const handleDropdownClose = () => {
+//     setIsDropdownOpened(false);
+//   };
 
-  const handleDropdownToggle = (e: MouseEvent) => {
-    e.stopPropagation();
-    setIsDropdownOpened((prev) => {
-      return !prev;
-    });
-  };
+//   const handleDropdownToggle = (e: MouseEvent) => {
+//     e.stopPropagation();
+//     setIsDropdownOpened((prev) => {
+//       return !prev;
+//     });
+//   };
 
-  useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
-      if (!ref.current) return;
-      if (ref.current.contains(e.target as Node) && onClickInside) {
-        onClickInside(e);
-      }
+//   useEffect(() => {
+//     const handleClick = (e: MouseEvent) => {
+//       if (!ref.current) return;
+//       if (ref.current.contains(e.target as Node) && onClickInside) {
+//         onClickInside(e);
+//       }
 
-      handleDropdownClose();
-    };
+//       handleDropdownClose();
+//     };
 
-    document.addEventListener('click', handleClick);
-    return () => {
-      document.removeEventListener('click', handleClick);
-    };
-  }, [ref, onClickInside]);
+//     document.addEventListener('click', handleClick);
+//     return () => {
+//       document.removeEventListener('click', handleClick);
+//     };
+//   }, [ref, onClickInside]);
 
-  return { ref, isDropdownOpened, handleDropdownOpen, handleDropdownClose, handleDropdownToggle };
-};
+//   return { ref, isDropdownOpened, handleDropdownOpen, handleDropdownClose, handleDropdownToggle };
+// };
 
-export default useDropdown;
+// export default useDropdown;
