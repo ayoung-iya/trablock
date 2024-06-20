3d8b2c518be4afc1bb72712478509fcd6fdd80fa
HaeJungg <gpwnd0120@gmail.com>
2024-06-20 15:49:46 +0900

    chore: add eslint

--- src/components/card/TravelCard.tsx
+++ src/components/card/TravelCard.tsx
@@ -72,6 +72,7 @@ export default function TravelCard({
     return () => {
       document.removeEventListener('click', handleClickOutside);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [menuVisible]);
 
   return (
