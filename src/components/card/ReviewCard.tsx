0d02ef2b1690c4cf1644c67a8e17f32f562a022e
HaeJungg <gpwnd0120@gmail.com>
2024-06-20 15:48:52 +0900

    design: 리뷰 컴포넌트 UI

--- src/components/card/ReviewCard.tsx
+++ src/components/card/ReviewCard.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+
 import React from 'react';
 
 import ImageBox from '@/components/common/ImageBox';
@@ -25,29 +27,48 @@ export default function ReviewCard({
   type = 'default',
   onClick
 }: ReviewCardProps) {
-  const cityString = city.join(' , ');
+  const cityString = city.join(', ');
+
+  const getClassNames = () => {
+    if (type === 'main') {
+      return 'w-[285px] h-[220px] sm:w-[320px] sm:h-[240px] md:w-[345px] md:h-[254px] flex-shrink-0';
+    }
+    return 'w-[156px] h-[156px] sm:w-[228px] sm:h-[228px] md:w-[236px] md:h-[236px] flex-shrink-0';
+  };
 
   return (
-    <button className="relative size-full max-h-80 max-w-80 overflow-hidden" type="button" onClick={onClick}>
-      <ImageBox className="size-full" src={imageUrl} alt={imageUrl} width={80} height={80} />
-      <div className="text-white absolute bottom-4 left-4">
-        <p>{title}</p>
+    <button
+      className={`relative overflow-hidden rounded-lg bg-gray-200 shadow-[0_0_10px_0_rgba(0,0,0,0.08)] ${getClassNames()} h-full w-full`}
+      type="button"
+      onClick={onClick}
+    >
+      <ImageBox className="h-full w-full" src={imageUrl} alt={imageUrl} width={80} height={80} />
+      <div className=" absolute bottom-4 left-4">
+        <p className="font-subtitle-1 text-white-01">{title}</p>
         {type === 'default' && (
           <>
-            <p>{cityString}</p>
-            <p>
+            <p className="font-subtitle-3 hidden text-gray-02 sm:block md:block">{cityString}</p>
+            <p className="font-subtitle-3 hidden text-gray-02 sm:block md:block">
               {startAt} ~ {endAt}
             </p>
           </>
         )}
         {type === 'main' && (
           <>
-            <p>{cityString}</p>
-            <div className="flex-row-center gap-2">
-              <div className="relative size-full max-h-8 max-w-8 overflow-hidden rounded-full">
-                <ImageBox className="size-full" src={profileImageUrl} alt="profileImageUrl" width={8} height={8} />
+            <p className="font-subtitle-3 text-gray-02">{cityString}</p>
+            <div className=" mt-3 flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <div className="relative h-8 w-8 overflow-hidden rounded-full">
+                  <ImageBox
+                    className="h-full w-full"
+                    src={profileImageUrl}
+                    alt="profileImageUrl"
+                    width={32}
+                    height={32}
+                  />
+                </div>
+                <span className="font-caption-2 text-gray-02">{name}</span>
               </div>
-              <span>{name}</span>
             </div>
           </>
         )}
