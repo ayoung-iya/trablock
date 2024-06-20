e9f79ea8aac2420a782e0ffdf2f3d585d4d6a550
HaeJungg <gpwnd0120@gmail.com>
2024-06-20 13:52:18 +0900

    fix: 리뷰카드 데이터 수정

--- src/app/(dashboard)/cardtest/page.tsx
+++ src/app/(dashboard)/cardtest/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 // import CircleCard from '@/components/card/CircleCard';
-// import ReviewCard from '@/components/card/ReviewCard';
+import ReviewCard from '@/components/card/ReviewCard';
 import TravelCard from '@/components/card/TravelCard';
 // import LogoSvg from '@/icons/logo.svg';
 
@@ -11,14 +11,24 @@ export default function Home() {
       imageUrl: 'https://picsum.photos/300/300',
       title: '보라카이'
     },
-    review: {
+    reviewDefault: {
       imageUrl: 'https://picsum.photos/800/800',
       title: '가족과 함께한 일본 여행',
-      route: ['도쿄', '긴자', '오사카'],
-      user: {
-        name: '트래블',
-        profileImg: 'https://picsum.photos/100/100'
-      }
+      city: ['오사카'],
+      name: '트래블',
+      profileImageUrl: 'https://picsum.photos/100/100',
+      startAt: '2024.8.12',
+      endAt: '2024.8.16',
+      onClick: () => alert('카드를 클릭했습니다!')
+    },
+    reviewMain: {
+      imageUrl: 'https://picsum.photos/800/800',
+      title: '가족과 함께한 일본 여행',
+      city: ['도쿄', '긴자', '오사카'],
+      name: '트래블',
+      profileImageUrl: 'https://picsum.photos/100/100',
+      type: 'main' as const,
+      onClick: () => alert('카드를 클릭했습니다!')
     },
     travel: {
       id: '1',
@@ -44,7 +54,8 @@ export default function Home() {
     <div className="flex-col-center gap-12 p-8">
       {/* <LogoSvg className="h-32 w-60 border border-solid" /> */}
       <div className="flex-row-center gap-4">{/* <CircleCard {...cardList.circle} /> */}</div>
-      {/* <ReviewCard {...cardList.review} /> */}
+      <ReviewCard {...cardList.reviewDefault} />
+      <ReviewCard {...cardList.reviewMain} />
       <TravelCard {...cardList.travel} />
     </div>
   );
--- src/components/card/ReviewCard.tsx
+++ src/components/card/ReviewCard.tsx
@@ -1,31 +1,56 @@
 import React from 'react';
 
-import { DefaultCardProps } from '@/components/card/type';
 import ImageBox from '@/components/common/ImageBox';
 
-export interface ReviewCardProps extends DefaultCardProps {
-  route: string[];
-  user: { name: string; profileImg: string };
+export interface ReviewCardProps {
+  imageUrl: string;
+  title: string;
+  city: string[];
+  name: string;
+  profileImageUrl: string;
+  startAt?: string;
+  endAt?: string;
+  type?: 'default' | 'main';
+  onClick: () => void;
 }
 
-export default function ReviewCard({ imageUrl, title, route, user, onClick }: ReviewCardProps) {
+export default function ReviewCard({
+  imageUrl,
+  title,
+  city,
+  name,
+  profileImageUrl,
+  startAt,
+  endAt,
+  type = 'default',
+  onClick
+}: ReviewCardProps) {
+  const cityString = city.join(' , ');
+
   return (
     <button className="relative size-full max-h-80 max-w-80 overflow-hidden" type="button" onClick={onClick}>
       <ImageBox className="size-full" src={imageUrl} alt={imageUrl} width={80} height={80} />
       <div className="text-white absolute bottom-4 left-4">
         <p>{title}</p>
-        {route.map((item, idx) => (
+        {type === 'default' && (
           <>
-            <span key={item}>{item}</span>
-            {idx < route.length - 1 && <span> → </span>}
+            <p>{cityString}</p>
+            <p>
+              {startAt} ~ {endAt}
+            </p>
           </>
-        ))}
-        <div className="flex-row-center gap-2">
-          <div className="relative size-full max-h-8 max-w-8 overflow-hidden rounded-full">
-            <ImageBox className="size-full" src={user.profileImg} alt={user.profileImg} width={8} height={8} />
-          </div>
-          <span>{user.name}</span>
-        </div>
+        )}
+        {type === 'main' && (
+          <>
+            <p>{cityString}</p>
+            <div className="flex-row-center gap-2">
+              <div className="relative size-full max-h-8 max-w-8 overflow-hidden rounded-full">
+                <ImageBox className="size-full" src={profileImageUrl} alt="profileImageUrl" width={8} height={8} />
+              </div>
+              <span>{name}</span>
+            </div>
+          </>
+        )}
       </div>
     </button>
   );
