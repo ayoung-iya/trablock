82456c18e1c9f94df84098f4d921099d301d0ea5
HaeJungg <gpwnd0120@gmail.com>
2024-06-20 15:29:45 +0900

    desing: 여행 카드 UI

--- src/components/card/TravelCard.tsx
+++ src/components/card/TravelCard.tsx
@@ -37,7 +37,6 @@ export default function TravelCard({
   name,
   profileImageUrl,
   thumbnailImageUrl,
-  price,
   bookmarkCount,
   isBookmarked,
   isEditable = false,
@@ -50,7 +49,7 @@ export default function TravelCard({
 
   const buttonStyle = isMd ? { maxHeight: `${divHeight}px` } : undefined;
 
-  const combinedTags = [travelCompanion, ...travelStyle, `₩${price}`];
+  const combinedTags = [travelCompanion, ...travelStyle];
 
   const handleMenuClick = (e: React.MouseEvent) => {
     e.stopPropagation();
@@ -76,32 +75,40 @@ export default function TravelCard({
   }, [menuVisible]);
 
   return (
-    <button
-      className="md:flex-row-center relative flex size-full overflow-hidden border border-solid max-md:flex-col"
+    <div
+      className="md:flex-row-center relative flex size-full overflow-hidden rounded-lg bg-white-01 shadow-[0_0_10px_0_rgba(0,0,0,0.08)] max-md:flex-col"
       style={buttonStyle}
-      type="button"
+      role="button"
+      tabIndex={0}
       onClick={onClick}
+      onKeyPress={onClick}
     >
       {/* 책갈피 아이콘 */}
-      <div className="absolute left-0 top-0 rounded-full border-2 border-gray-300 p-2">
+      <div className="absolute left-4 top-4 h-9 w-9 flex-shrink-0 rounded-[5px] bg-white-01 p-2 backdrop-blur-[10px]">
         {isBookmarked ? (
-          <img src="/icons/bookmark-filled.svg" alt="bookmarked" />
+          <ImageBox
+            className="h-full w-full"
+            src="/icons/bookmark-filled.svg"
+            alt="bookmarked"
+            width={18}
+            height={18}
+          />
         ) : (
-          <img src="/icons/bookmark.svg" alt="not bookmarked" />
+          <ImageBox className="h-full w-full" src="/icons/bookmark.svg" alt="not bookmarked" width={18} height={18} />
         )}
       </div>
       {/* 케밥 메뉴 */}
       {isEditable && isPlanTab && (
-        <div className="absolute right-2 top-2">
-          <button type="button" onClick={handleMenuClick}>
-            <img src="/icons/kebab.svg" alt="kebab menu" />
+        <div className="absolute right-4 top-4">
+          <button type="button" onClick={handleMenuClick} className="flex h-4 w-4 items-center justify-center">
+            <ImageBox className="h-full" src="/icons/kebab.svg" alt="kebab menu" width={16} height={16} />
           </button>
           {menuVisible && (
-            <div className="bg-white absolute right-0 mt-2 w-24 rounded-md shadow-lg">
-              <button type="button" className="block w-full px-4 py-2 text-left text-gray-700">
+            <div className="absolute right-0 mt-2 flex w-[110px] flex-col gap-4 rounded-md bg-white-01 p-4 shadow-[0_0_10px_0_rgba(0,0,0,0.1)]">
+              <button type="button" className="font-btn-text block w-full text-left text-black-01">
                 수정하기
               </button>
-              <button type="button" className="block w-full px-4 py-2 text-left text-red-600">
+              <button type="button" className="font-btn-text block w-full text-left text-red-01">
                 삭제하기
               </button>
             </div>
@@ -119,18 +126,21 @@ export default function TravelCard({
       {/* 콘텐츠 */}
       <div className="flex-grow max-md:w-full" ref={divRef}>
         {/* 상단 각종 데이터 */}
-        <div className="flex flex-col gap-2 border-b p-4">
-          <p>{title}</p>
-          <div>
-            <div className="flex items-center gap-1">
-              <img src="/icons/map-pin.svg" alt="location" className="h-4 w-4" />
-              {city.join(', ')}
+        <div className="flex flex-col gap-2 border-b border-gray-03 p-4">
+          <p className="font-subtitle-1">{title}</p>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-[6px]">
+              <ImageBox src="/icons/map-pin.svg" alt="location" className="h-4 w-4" width={16} height={16} />
+              <span className="font-subtitle-3 text-gray-01">{city.join(', ')}</span>
             </div>
-            <div className="flex items-center gap-1">
-              <img src="/icons/calendar.svg" alt="calendar" className="h-4 w-4" />
-              {startAt} ~ {endAt}
+            <div className="flex items-center gap-[6px]">
+              <ImageBox src="/icons/calendar.svg" alt="calendar" className="h-4 w-4" width={16} height={16} />
+              <span className="font-subtitle-3 text-gray-01">
+                {startAt} ~ {endAt}
+              </span>
             </div>
           </div>
+          {/* 추후 컴포넌트 추가 */}
           <div className="flex-row-center flex-wrap gap-2">
             {combinedTags.map((item) => (
               <p key={item} className="flex-shrink-0 bg-slate-300">
@@ -139,8 +149,7 @@ export default function TravelCard({
             ))}
           </div>
         </div>
-        {/* 하단 프로필, 카운트 */}
-        <div className="flex-row-center justify-between p-3">
+        <div className="flex-row-center justify-between p-4">
           <div className="flex-row-center gap-2">
             <ImageBox
               className="size-full max-h-8 max-w-8 rounded-full"
@@ -149,15 +158,16 @@ export default function TravelCard({
               width={8}
               height={8}
             />
-            <p>{name}</p>
+            <p className="font-caption-2 text-black-01">{name}</p>
           </div>
-          <div className="flex-row-center gap-2">
-            <div>
-              <img src="/icons/bookmark-filled.svg" alt="bookmark count" className="inline h-4 w-4" /> {bookmarkCount}
+          <div className="flex-row-center gap-1 p-1">
+            <div className="inline h-[11px] w-[11px]">
+              <ImageBox src="/icons/bookmark-gray.svg" alt="bookmark count" className="inline" width={11} height={11} />
             </div>
+            <span className="font-caption-3 text-gray-01">{bookmarkCount}</span>
           </div>
         </div>
       </div>
-    </button>
+    </div>
   );
 }
