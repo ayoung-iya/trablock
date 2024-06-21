97a7ed5da1252730727295e857e24a68e6a2281a
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-06-21 12:21:38 +0900

    T6-123 Add representative Image and UI fix

--- src/components/textEditor/LoadingComponent.tsx
+++ src/components/textEditor/LoadingComponent.tsx
@@ -1,20 +1,18 @@
-// IconComponent.jsx
+// LoadingComponent.jsx
 import React from 'react';
 
 function LoadingComponent() {
   return (
-    <div className="flex h-[370px] w-full items-center justify-center ">
+    <div className="flex h-[370px] w-full items-center justify-center">
       <svg
         fill="#000000"
         version="1.1"
         id="Capa_1"
         xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
+        className="h-6 w-6 animate-spin"
         width="800px"
         height="800px"
         viewBox="0 0 26.349 26.35"
-        xmlSpace="preserve"
-        className="h-6 w-6 animate-spin"
       >
         <g>
           <g>
@@ -24,7 +22,10 @@ function LoadingComponent() {
             <circle cx="21.365" cy="21.363" r="1.541" />
             <circle cx="3.082" cy="13.792" r="2.465" />
             <circle cx="24.501" cy="13.791" r="1.232" />
-            <path d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z" />
+            <path
+              d="M4.694,19.84c-0.843,0.843-0.843,2.207,0,3.05c0.842,0.843,2.208,0.843,3.05,0
+                 c0.843-0.843,0.843-2.207,0-3.05"
+            />
             <circle cx="21.364" cy="6.218" r="0.924" />
           </g>
         </g>
