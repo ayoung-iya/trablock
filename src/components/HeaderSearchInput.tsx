659ef244e10c189e2c3f4c858c91757cc91224c6
ayoung-iya <rhaehfdl1879@naver.com>
2024-06-24 04:58:38 +0900

    refact: 미디어 쿼리로 반응형에 대응

--- src/components/HeaderSearchInput.tsx
+++ src/components/HeaderSearchInput.tsx
@@ -14,7 +14,7 @@ import selectMatchingCities from '@/libs/utils/selectMatchingCities';
 
 import ImageBox from './common/ImageBox';
 
-export default function HeaderSearchInput({ isMobile = false }: { isMobile?: boolean }) {
+export default function HeaderSearchInput() {
   const params = useSearchParams();
   const router = useRouter();
   const initialSearchString = params.get('keyword');
@@ -22,6 +22,7 @@ export default function HeaderSearchInput({ isMobile = false }: { isMobile?: boo
   const [searchString, setSearchString] = useState(initialSearchString || '');
   const [isShowInMobile, setIsShowInMobile] = useState(false);
   const filteredCities = searchString ? selectMatchingCities(searchString as string) : INITIAL_CITIES;
+
   const searchInputRef = useRef<HTMLInputElement>(null);
   const { ref, isDropdownOpened, handleDropdownOpen, handleDropdownClose } = useDropdown<HTMLUListElement>({
     onClickInside: () => {
@@ -56,122 +57,67 @@ export default function HeaderSearchInput({ isMobile = false }: { isMobile?: boo
     handleDropdownClose();
   };
 
-  if (isMobile) {
-    return (
-      <>
-        <button
-          type="button"
-          onClick={() => {
-            setIsShowInMobile(true);
-            searchInputRef.current?.focus();
-          }}
-        >
-          <SearchIcon className="size-[22px] md:hidden" fill="#2d3136" />
-        </button>
-
-        <form
-          onSubmit={handleSubmit}
-          // eslint-disable-next-line max-len
-          className={`fixed inset-0 z-20 bg-white-01 px-5 pb-[8px] pt-3 transition-all duration-500 md:hidden ${isShowInMobile ? '' : 'translate-x-full'}`}
-        >
-          <div className="flex-row-center gap-3">
-            <ImageBox
-              className="size-[18px]"
-              src={arrowIcon}
-              alt="뒤로 가기"
-              width={18}
-              height={18}
-              onClick={() => {
-                handleSearchStringReset();
-                setIsShowInMobile(false);
-              }}
-            />
-            <div className="flex-row-center h-10 w-full rounded-[5px] bg-gray-03 px-3">
-              <input
-                type="text"
-                value={searchString}
-                onChange={handleSearchStringChange}
-                className="w-full bg-inherit text-sm leading-[180%] focus:outline-none"
-                placeholder="여행 계획을 검색해보세요!"
-                ref={searchInputRef}
-              />
-              <button type="submit">
-                <SearchIcon className="size-[18px]" fill="#2d3136" />
-              </button>
-            </div>
-          </div>
-          <ul className="my-5 max-h-[90vh] overflow-y-auto md:hidden">
-            {filteredCities?.map((city) => {
-              const splitCityName = city.split(searchString);
-              const cityName =
-                splitCityName.length > 1 && searchString ? (
-                  <>
-                    {splitCityName[0]}
-                    <b>{searchString}</b>
-                    {splitCityName[1]}
-                  </>
-                ) : (
-                  city
-                );
-
-              return (
-                <li>
-                  <Link
-                    href={`/search?keyword=${city}`}
-                    onClick={() => {
-                      setSearchString(city);
-                      setIsShowInMobile(false);
-                    }}
-                    className="flex-row-center h-[41px] cursor-pointer gap-3 py-2 hover:bg-gray-03"
-                  >
-                    <SearchIcon className="size-[18px] md:hidden" fill="#2d3136" />
-                    <span className="text-sm leading-[180%]">{cityName}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </form>
-      </>
-    );
-  }
-
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="relative ml-[71px] mr-[47px] hidden w-full min-w-80 max-w-[400px] md:block"
-    >
-      <div className="flex-row-center gap-3">
-        <div className="flex-row-center h-10 w-full rounded-[5px] bg-gray-03 px-3">
-          <input
-            type="text"
-            value={searchString}
-            onChange={handleSearchStringChange}
-            className="w-full bg-inherit text-sm leading-[180%] focus:outline-none"
-            placeholder="여행 계획을 검색해보세요!"
-            ref={searchInputRef}
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          setIsShowInMobile(true);
+          searchInputRef.current?.focus();
+        }}
+      >
+        <SearchIcon className="size-[22px] fill-black-01 md:hidden" />
+      </button>
+
+      <form
+        onSubmit={handleSubmit}
+        // eslint-disable-next-line max-len
+        className={`fixed inset-0 z-20 bg-white-01 px-5 pb-[8px] pt-3 transition-all duration-500 md:relative md:ml-[71px] md:mr-[47px] md:w-full md:min-w-80 md:max-w-[400px] md:translate-x-0 md:p-0 ${isShowInMobile ? '' : 'translate-x-full'}`}
+      >
+        <div className="flex-row-center gap-3">
+          <ImageBox
+            className="size-[18px] md:hidden"
+            src={arrowIcon}
+            alt="뒤로 가기"
+            width={18}
+            height={18}
+            onClick={() => {
+              handleSearchStringReset();
+              setIsShowInMobile(false);
+            }}
           />
-          {searchString && (
-            <button
-              type="button"
-              className="mr-[9px]"
-              onClick={() => {
-                handleSearchStringReset();
-                searchInputRef.current?.focus();
-              }}
-            >
-              <XCircleFilledIcon className="size-4" fill="#a0aab8" />
+          <div className="flex-row-center h-10 w-full rounded-[5px] bg-gray-03 px-3">
+            <input
+              type="text"
+              value={searchString}
+              onChange={handleSearchStringChange}
+              className="w-full bg-inherit text-sm leading-[180%] focus:outline-none"
+              placeholder="여행 계획을 검색해보세요!"
+              ref={searchInputRef}
+            />
+            {searchString && (
+              <button
+                type="button"
+                className="mr-[9px]"
+                onClick={() => {
+                  handleSearchStringReset();
+                  searchInputRef.current?.focus();
+                }}
+              >
+                <XCircleFilledIcon className="size-4 fill-black-01 md:fill-gray-01" fill="#a0aab8" />
+              </button>
+            )}
+            <button type="submit">
+              <SearchIcon className="size-[18px] fill-black-01 md:fill-gray-01" fill="#2d3136" />
             </button>
-          )}
-          <button type="submit">
-            <SearchIcon className="size-[18px]" fill="#a0aab8" />
-          </button>
+          </div>
         </div>
-      </div>
-      {isDropdownOpened && (
-        <ul className="shadow-box absolute top-[52px] z-10 max-h-44 w-full overflow-y-auto" ref={ref}>
+        <ul
+          // eslint-disable-next-line max-len
+          className={`md:shadow-box my-5 max-h-[90vh] overflow-y-auto ${isDropdownOpened ? 'md:absolute' : 'md:hidden'} md:top-[52px] md:z-10 md:m-0 md:max-h-44 md:w-full md:overflow-y-auto`}
+          ref={ref}
+        >
           {filteredCities?.map((city) => {
-            // 입력한 부분과 일치하는 글자는 굵게 하기 위해서
             const splitCityName = city.split(searchString);
             const cityName =
               splitCityName.length > 1 && searchString ? (
@@ -190,18 +136,21 @@ export default function HeaderSearchInput({ isMobile = false }: { isMobile?: boo
                   href={`/search?keyword=${city}`}
                   onClick={() => {
                     setSearchString(city);
-                    handleDropdownClose();
+                    setIsShowInMobile(false);
                   }}
-                  className="flex-row-center h-11 cursor-pointer gap-3 px-4 py-3 hover:bg-gray-03"
+                  // eslint-disable-next-line max-len
+                  className="flex-row-center h-[41px] cursor-pointer gap-3 rounded-[5px] px-4 py-2 hover:bg-gray-03 md:h-11 md:py-3"
                 >
-                  <SearchIcon className="size-5" fill="#a0aab8" />
-                  <span className="text-base font-medium leading-[125%]">{cityName}</span>
+                  <SearchIcon className="size-[18px] fill-black-01 md:size-5 md:fill-gray-01" fill="#2d3136" />
+                  <span className="text-sm leading-[180%] md:text-base md:font-medium md:leading-[125%]">
+                    {cityName}
+                  </span>
                 </Link>
               </li>
             );
           })}
         </ul>
-      )}
-    </form>
+      </form>
+    </>
   );
 }
