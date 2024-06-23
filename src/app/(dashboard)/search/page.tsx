a249fe7be6ac77f9d3256721b171b1ea95cb778e
ayoung-iya <rhaehfdl1879@naver.com>
2024-06-23 18:32:21 +0900

    feat: 검색페이지 api 연결 및 검색 결과 리스트 ui 생성

--- src/app/(dashboard)/search/page.tsx
+++ src/app/(dashboard)/search/page.tsx
@@ -1,19 +1,50 @@
 'use client';
 
+import { useEffect } from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import useGetSearch from '@/apis/useSearch/useGetSearch';
+import TravelCard from '@/components/card/TravelCard';
 import OrderFilterSection from '@/components/OrderFilterSection';
+import useIntersectingState from '@/libs/hooks/useIntersectingState';
 
 export default function Search({ searchParams }: { searchParams: { [key: string]: string } }) {
-  const { keyword } = searchParams;
+  const { keyword = '', order } = searchParams;
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useGetSearch(keyword, order);
+  const router = useRouter();
+
+  const [isIntersecting, ref] = useIntersectingState<HTMLLIElement>();
+
+  const linkArticleDetailPage = (articleId: string) => () => router.push(`/plan/detail/${articleId}`);
+
+  useEffect(() => {
+    if (!isIntersecting || isFetchingNextPage) {
+      return;
+    }
+
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  }, [isIntersecting, hasNextPage, fetchNextPage, isFetchingNextPage]);
 
   return (
     <div className="mx-5 my-5">
       <h1 className="font-title-2 md:font-title-3 mb-3 whitespace-nowrap">
-        &lsquo;{decodeURIComponent(keyword)}&rsquo; 여행 계획 검색 결과
+        {keyword ? `‘${decodeURIComponent(keyword)}’` : '전체'} 여행 계획 검색 결과
       </h1>
       <div className="flex-row-center justify-between">
-        <span className="font-caption-1 flex-shrink-0 md:text-lg">전체 50개</span>
+        <span className="font-caption-1 flex-shrink-0 md:text-lg">전체 {data?.totalElements || 0}개</span>
         <OrderFilterSection />
       </div>
+      <ul className="mt-5 flex flex-wrap gap-[18px] md:gap-5">
+        {data?.pages.map((page) => {
+          return page.map(({ articleId, ...rest }) => {
+            return <TravelCard id={articleId} {...rest} onClick={linkArticleDetailPage(articleId)} />;
+          });
+        })}
+        {hasNextPage && !isFetchingNextPage && <li className="h-20 w-full" ref={ref} />}
+      </ul>
     </div>
   );
 }
