3cf3468e3c9874f82ee604514e071c7e850ff669
ayoung-iya <rhaehfdl1879@naver.com>
2024-06-24 05:00:22 +0900

    refact: searchInput GNB에 적용

--- src/components/common/GNB.tsx
+++ src/components/common/GNB.tsx
@@ -1,32 +1,59 @@
 import { cookies, headers } from 'next/headers';
+import Image from 'next/image';
+import Link from 'next/link';
 
+import HeaderSearchInput from '@/components/HeaderSearchInput';
+import calendarSvg from '@/icons/calendar-add.svg?url';
 import LogoSvg from '@/icons/logo.svg';
-import SearchSvg from '@/icons/search.svg';
+import ProfileDefault from '@/icons/profile-default.svg';
+
+import ImageBox from './ImageBox';
 
 export default function GNB() {
   const cookieStore = cookies();
-  const hasCookie = cookieStore.has('user');
-  // 추후 쿠키 저장 이름에 따라 달라질 예정
+  const hasCookie = cookieStore.has('authorization-token');
 
   const headerList = headers();
   const skipIcons = headerList.get('X-Skip-Icons') === 'true';
-  // path 가 메인일 때는 검색 아이콘 나타나지 않음
+  // imageurl 받아야함.
+  const displayImageUrl = '';
 
   return (
-    <nav className="flex-row-center h-[3.75rem] w-full justify-between bg-gray-01 p-3.5 md:h-[4.5rem]">
-      <LogoSvg width={123} height={32} />
-      <ul className="flex gap-3.5">
-        <li>{!skipIcons && <SearchSvg width={24} height={24} />}</li>
-        <li>
-          <button type="button">계획 생성하기</button>
-        </li>
-        {hasCookie && <li>프로필 사진 컴포넌트</li>}
-        {!hasCookie && (
-          <li>
-            <button type="button">로그인</button>
+    <nav className="flex-row-center h-[3.75rem] w-full justify-between p-10 md:h-[4.5rem]">
+      <Link href="/">
+        <LogoSvg width={123} height={32} />
+      </Link>
+      <div className="flex w-full justify-end gap-5 md:justify-between md:gap-0">
+        {!skipIcons && <HeaderSearchInput />}
+        <ul className="flex-row-center gap-5">
+          <li className="flex-row-center">
+            <button type="button">
+              <span className="font-btn-2 hidden whitespace-nowrap text-primary-01 md:block">계획 생성하기</span>
+              <ImageBox
+                src={calendarSvg}
+                alt="일정 추가하기"
+                className="size-[22px] md:hidden"
+                width={22}
+                height={22}
+              />
+            </button>
           </li>
-        )}
-      </ul>
+          {hasCookie && (
+            <Link href="/profile">
+              <li>
+                <Image src={displayImageUrl} alt="Profile Image" width={36} height={36} className="rounded-full" />
+              </li>
+            </Link>
+          )}
+          {!hasCookie && (
+            <Link href="/login">
+              <li>
+                <ProfileDefault width={36} height={36} />
+              </li>
+            </Link>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 }
