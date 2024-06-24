46fb38c11ec3e2b02ca77cecad7620618601d5c9
ayoung-iya <rhaehfdl1879@naver.com>
2024-06-24 13:23:13 +0900

    refact: 이벤트 핸들러 & 함수 분리

--- src/components/HeaderSearchInput.tsx
+++ src/components/HeaderSearchInput.tsx
@@ -14,6 +14,19 @@ import selectMatchingCities from '@/libs/utils/selectMatchingCities';
 
 import ImageBox from './common/ImageBox';
 
+const matchStringBold = (city: string, matchString: string) => {
+  const splitCityName = city.split(matchString);
+  return splitCityName.length > 1 && matchString ? (
+    <>
+      {splitCityName[0]}
+      <b>{matchString}</b>
+      {splitCityName[1]}
+    </>
+  ) : (
+    city
+  );
+};
+
 export default function HeaderSearchInput() {
   const params = useSearchParams();
   const router = useRouter();
@@ -41,6 +54,31 @@ export default function HeaderSearchInput() {
     setSearchString(initialSearchString || '');
   };
 
+  const handleCloseSearchList = () => {
+    handleDropdownClose();
+    setIsShowInMobile(false);
+  };
+
+  const handleXButtonClick = () => {
+    setSearchString('');
+    searchInputRef.current?.focus();
+  };
+
+  const handleLinkClick = (city: string) => () => {
+    setSearchString(city);
+    handleCloseSearchList();
+  };
+
+  const handleMobileHide = () => {
+    handleSearchStringReset();
+    handleCloseSearchList();
+  };
+
+  const handleMobileShow = () => {
+    setIsShowInMobile(true);
+    searchInputRef.current?.focus();
+  };
+
   const handleSearchStringChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     if (!e.target.value && !searchString) {
       handleDropdownClose();
@@ -59,16 +97,9 @@ export default function HeaderSearchInput() {
 
   return (
     <>
-      <button
-        type="button"
-        onClick={() => {
-          setIsShowInMobile(true);
-          searchInputRef.current?.focus();
-        }}
-      >
+      <button type="button" onClick={handleMobileShow}>
         <SearchIcon className="size-[22px] fill-black-01 md:hidden" />
       </button>
-
       <form
         onSubmit={handleSubmit}
         // eslint-disable-next-line max-len
@@ -81,10 +112,7 @@ export default function HeaderSearchInput() {
             alt="뒤로 가기"
             width={18}
             height={18}
-            onClick={() => {
-              handleSearchStringReset();
-              setIsShowInMobile(false);
-            }}
+            onClick={handleMobileHide}
           />
           <div className="flex-row-center h-10 w-full rounded-[5px] bg-gray-03 px-3">
             <input
@@ -96,19 +124,12 @@ export default function HeaderSearchInput() {
               ref={searchInputRef}
             />
             {searchString && (
-              <button
-                type="button"
-                className="mr-[9px]"
-                onClick={() => {
-                  handleSearchStringReset();
-                  searchInputRef.current?.focus();
-                }}
-              >
-                <XCircleFilledIcon className="size-4 fill-black-01 md:fill-gray-01" fill="#a0aab8" />
+              <button type="button" className="mr-[9px]" onClick={handleXButtonClick}>
+                <XCircleFilledIcon className="size-4 fill-black-01 md:fill-gray-01" />
               </button>
             )}
             <button type="submit">
-              <SearchIcon className="size-[18px] fill-black-01 md:fill-gray-01" fill="#2d3136" />
+              <SearchIcon className="size-[18px] fill-black-01 md:fill-gray-01" />
             </button>
           </div>
         </div>
@@ -117,38 +138,21 @@ export default function HeaderSearchInput() {
           className={`md:shadow-box my-5 max-h-[90vh] overflow-y-auto ${isDropdownOpened ? 'md:absolute' : 'md:hidden'} md:top-[52px] md:z-10 md:m-0 md:max-h-44 md:w-full md:overflow-y-auto`}
           ref={ref}
         >
-          {filteredCities?.map((city) => {
-            const splitCityName = city.split(searchString);
-            const cityName =
-              splitCityName.length > 1 && searchString ? (
-                <>
-                  {splitCityName[0]}
-                  <b>{searchString}</b>
-                  {splitCityName[1]}
-                </>
-              ) : (
-                city
-              );
-
-            return (
-              <li>
-                <Link
-                  href={`/search?keyword=${city}`}
-                  onClick={() => {
-                    setSearchString(city);
-                    setIsShowInMobile(false);
-                  }}
-                  // eslint-disable-next-line max-len
-                  className="flex-row-center h-[41px] cursor-pointer gap-3 rounded-[5px] px-4 py-2 hover:bg-gray-03 md:h-11 md:py-3"
-                >
-                  <SearchIcon className="size-[18px] fill-black-01 md:size-5 md:fill-gray-01" fill="#2d3136" />
-                  <span className="text-sm leading-[180%] md:text-base md:font-medium md:leading-[125%]">
-                    {cityName}
-                  </span>
-                </Link>
-              </li>
-            );
-          })}
+          {filteredCities?.map((city) => (
+            <li>
+              <Link
+                href={`/search?keyword=${city}`}
+                onClick={handleLinkClick(city)}
+                // eslint-disable-next-line max-len
+                className="flex-row-center h-[41px] cursor-pointer gap-3 rounded-[5px] px-4 py-2 hover:bg-gray-03 md:h-11 md:py-3"
+              >
+                <SearchIcon className="size-[18px] fill-black-01 md:size-5 md:fill-gray-01" fill="#2d3136" />
+                <span className="text-sm leading-[180%] md:text-base md:font-medium md:leading-[125%]">
+                  {matchStringBold(city, searchString)}
+                </span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </form>
     </>
