2a1f235b0a92515c6b70ec5be9335d2a4e107e92
HaeJungg <gpwnd0120@gmail.com>
2024-06-25 07:51:32 +0900

    feat: 리뷰 베너 수정

--- src/app/(dashboard)/main/page.tsx
+++ src/app/(dashboard)/main/page.tsx
@@ -3,12 +3,14 @@
 import React from 'react';
 
 import BannerArticleList from '@/components/BannerArticleList';
+import BannerReviewList from '@/components/BannerReviewList';
 
 export default function Page() {
   return (
     <div className="container mx-auto">
       <h1 className="my-4 text-2xl font-bold">Banner Articles</h1>
       <BannerArticleList />
+      <BannerReviewList />
     </div>
   );
 }
