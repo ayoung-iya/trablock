3594205837bc00d59dc9cdeb40b8cc20f784a9ac
kong33 <96658105+kong33@users.noreply.github.com>
2024-06-24 23:59:30 +0900

    Revert "Refactor: kakaologin"

--- src/components/KakaoLogin.tsx
+++ src/components/KakaoLogin.tsx
@@ -8,6 +8,7 @@ import kakaoButton from '@/icons/kakaoLogin.png';
 export default function KakaoLogin() {
   const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
   const KAKAO_REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
   const loginHandler = () => {
