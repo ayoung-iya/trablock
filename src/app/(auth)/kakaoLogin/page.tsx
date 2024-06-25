e80f980bf6820d492431fab5be750b97d83bc739
gaeun <kge0211114@gmail.com>
2024-06-25 10:40:50 +0900

    feat: 충돌 해결2

--- src/app/(auth)/kakaoLogin/page.tsx
+++ src/app/(auth)/kakaoLogin/page.tsx
@@ -1,3 +1,80 @@
+// 'use client';
+
+// import { useEffect } from 'react';
+// import Cookies from 'js-cookie';
+// import { useRouter } from 'next/router';
+
+// import usePostOauthLogin from '@/apis/oauthLogin/usePostOauthLogin';
+// import usePostKakaoUserData from '@/apis/useGetKakaoUserData/usePostKakaoUserData';
+// import usePostKakaoToken from '@/apis/usePostKakao/usePostKakaoToken';
+// import useManageKakaoLogin from '@/libs/hooks/useManageKakaoLogin';
+
+// export default function Kakaologin() {
+//   const { code } = useManageKakaoLogin();
+
+//   const postKakaoTokenMutate = usePostKakaoToken();
+//   const postKakaoUserData = usePostKakaoUserData();
+//   const postOauthData = usePostOauthLogin();
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     if (code) {
+//       console.log(code);
+//       postKakaoTokenMutate.mutate(code, {
+//         onSuccess: (response) => {
+//           const { access_token: accessToken } = response.data;
+//           Cookies.set('kakao', accessToken);
+//           console.log('토큰 성공');
+//           console.log(accessToken);
+//           postKakaoUserData.mutate(accessToken, {
+//             onSuccess: (res) => {
+//               console.log(res);
+//               const { kakao_account: kakaoAccount } = res.data;
+//               const { email, profile } = kakaoAccount;
+//               const { nickname, profile_image_url: profileImage } = profile;
+//               const payload = {
+//                 profile_nickname: nickname,
+//                 profile_image: profileImage,
+//                 account_email: email,
+//                 is_agreement: true
+//               };
+//               console.log(payload);
+//               postOauthData.mutate(payload, {
+//                 onSuccess: (responses) => {
+//                   console.log(responses, '오어스');
+//                   const authorizationToken = responses.headers['authorization-token'];
+//                   const expiresAt = responses.headers['authorization-token-expired-at'];
+//                   const refreshToken = responses.headers['refresh-token'];
+
+//                   if (authorizationToken && expiresAt && refreshToken) {
+//                     Cookies.set('authorization-token', authorizationToken, { secure: true });
+//                     Cookies.set('expires-at', expiresAt);
+//                     Cookies.set('refresh-token', refreshToken, { secure: true });
+//                   }
+//                   router.push('/');
+//                 },
+//                 onError: (err) => console.log('oauth', err)
+//               });
+//             },
+//             onError: (error) => {
+//               console.log('userdata', error);
+//             }
+//           });
+//         },
+//         onError: (err) => {
+//           console.log('token', err);
+//         }
+//       });
+//     }
+//   }, [code, postKakaoTokenMutate, postKakaoUserData, postOauthData, router]);
+
+//   return (
+//     <div>
+//       <p>kakaoLogin</p>
+//     </div>
+//   );
+// }
+
 'use client';
 
 import { useEffect } from 'react';
@@ -43,9 +120,9 @@ export default function Kakaologin() {
               postOauthData.mutate(payload, {
                 onSuccess: (responses) => {
                   console.log(responses, '오어스');
-                  const authorizationToken = responses.headers['authorization-token'];
-                  const expiresAt = responses.headers['authorization-token-expired-at'];
-                  const refreshToken = responses.headers['refresh-token'];
+                  const authorizationToken = responses.headers.get('authorization-token');
+                  const expiresAt = responses.headers.get('authorization-token-expired-at');
+                  const refreshToken = responses.headers.get('refresh-token');
 
                   if (authorizationToken && expiresAt && refreshToken) {
                     Cookies.set('authorization-token', authorizationToken, { secure: true });
