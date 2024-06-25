62a86ca7a1cad06cf525229d9acd19d122a40020
jinhok96 <jinhok96a@gmail.com>
2024-06-25 13:34:29 +0900

    style: layout 수정

--- src/app/(dashboard)/search/page.tsx
+++ src/app/(dashboard)/search/page.tsx
@@ -24,7 +24,7 @@ export default function Search({ searchParams }: { searchParams: { [key: string]
   }, [isIntersecting, hasNextPage, fetchNextPage, isFetchingNextPage]);
 
   return (
-    <div className="mx-5 my-5 max-w-[1200px]">
+    <div className="mx-5 my-5 max-w-[1200px] md:mx-auto md:px-7">
       <h1 className="font-title-2 md:font-title-3 mb-3 whitespace-nowrap">
         {keyword ? `‘${decodeURIComponent(keyword)}’` : '전체'} 여행 계획 검색 결과
       </h1>
@@ -34,7 +34,7 @@ export default function Search({ searchParams }: { searchParams: { [key: string]
         </span>
         <OrderFilterSection />
       </div>
-      <ul className="mt-5 flex flex-wrap gap-[18px] md:gap-5">
+      <ul className="mt-5 grid grid-cols-1 gap-[18px] md:gap-5 lg:grid-cols-2">
         {data?.pages.flat().map(({ articleId, ...rest }) => <TravelCard id={articleId} {...rest} isSearchPage />)}
         {hasNextPage && !isFetchingNextPage && <li className="h-20 w-full" ref={ref} />}
       </ul>
