3babb9347e2f5f0286ec977d52f854822870afe1
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-09 01:00:47 +0900

    refactor: 비밀번호 확인 validation 수정

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -118,8 +118,10 @@ export default function SignupForm() {
     });
   };
 
-  const handleCheckboxClick = () => {
-    setIsAgree((prev) => !prev);
+  const validatePasswordConfirm = (value: string) => {
+    const password = getValues('password');
+
+    return value === password || '비밀번호가 일치하지 않습니다.';
   };
 
   const registerList = {
