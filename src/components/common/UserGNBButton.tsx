d1510510fa14651153edee679a86814b15b111a7
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-11 22:00:43 +0900

    refactor: 서버 컴포넌트로 변경 및 상태 변경에 따라 필요한 부분은 클라이언트 컴포넌트로 분리

---
+++ src/components/common/UserGNBButton.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import useGetProfile from '@/apis/useProfileService/useGetProfile';
+import ImageBox from '@/components/common/ImageBox';
+import logout from '@/icons/logout.svg?url';
+import ProfileDefault from '@/icons/profile-default.svg?url';
+
+const DEFAULT_IMAGE_URL = ProfileDefault.src;
+
+export default function UserGNBButton({ hasAuthToken }: { hasAuthToken: boolean }) {
+  const authToken = Cookies.get('authorization-token') || '';
+  const userId = authToken ? jwtDecode<{ userId: number; exp: number }>(authToken)?.userId : '';
+
+  const { data: profileData } = useGetProfile(`${userId}`);
+  const [isLoggedIn, setIsLoggedIn] = useState(hasAuthToken);
+  const [profileImgURL, setProfileImgURL] = useState(DEFAULT_IMAGE_URL);
+  const router = useRouter();
+
+  const handleLogout: React.MouseEventHandler = () => {
+    Cookies.remove('authorization-token');
+    Cookies.remove('expires-at');
+    Cookies.remove('refresh-token');
+    setIsLoggedIn(false);
+
+    router.push('/');
+  };
+
+  useEffect(() => {
+    if (authToken) {
+      setIsLoggedIn(true);
+      setProfileImgURL(profileData?.profile_img_url || DEFAULT_IMAGE_URL);
+    } else {
+      setIsLoggedIn(false);
+      setProfileImgURL(DEFAULT_IMAGE_URL);
+    }
+  }, [authToken, profileData]);
+
+  if (isLoggedIn) {
+    return (
+      <>
+        <li className="flex-row-center">
+          <button type="button" className="size-6" onClick={handleLogout}>
+            <ImageBox src={logout} alt="로그아웃" className="size-6" width={24} height={24} />
+          </button>
+        </li>
+        <Link href={`/profile/${userId}`}>
+          <li>
+            <ImageBox src={profileImgURL} alt="Profile Image" className="size-9 rounded-full" width={36} height={36} />
+          </li>
+        </Link>
+      </>
+    );
+  }
+
+  return (
+    <Link href="/login">
+      <li>
+        <button type="button" className="btn-solid btn-sm">
+          로그인
+        </button>
+      </li>
+    </Link>
+  );
+}
