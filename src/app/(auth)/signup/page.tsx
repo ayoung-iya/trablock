20214cc97092a234cb7e24b5a001e80171c2268f
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-23 03:22:45 +0900

    feat: SEO 고려하여 Head 컴포넌트 추가

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 import KakaoLogin from '@/components/KakaoLogin';
@@ -6,21 +7,27 @@ import Logo from '@/icons/logo.svg';
 
 export default function signup() {
   return (
-    <div className="flex-col-center pb-32 pt-32">
-      <Logo width={123} height={32} />
-      <p className="font-signin-1 mt-5">
-        아이디가 있으신가요?
-        <span className="text-primary-01">
-          <Link href="/login"> 로그인</Link>
-        </span>
-      </p>
-      <p className="font-signin-2 mt-9 text-gray-01">SNS 계정으로 회원가입</p>
-      <div className="mt-3">
-        <KakaoLogin />
-      </div>
+    <>
+      <Head>
+        <title>회원가입 - 트래블록</title>
+      </Head>
 
-      <hr className="mt-9 w-80 border-gray-01" />
-      <SignupForm />
-    </div>
+      <div className="flex-col-center pb-32 pt-32">
+        <Logo width={123} height={32} />
+        <p className="font-signin-1 mt-5">
+          아이디가 있으신가요?
+          <span className="text-primary-01">
+            <Link href="/login"> 로그인</Link>
+          </span>
+        </p>
+        <p className="font-signin-2 mt-9 text-gray-01">SNS 계정으로 회원가입</p>
+        <div className="mt-3">
+          <KakaoLogin />
+        </div>
+
+        <hr className="mt-9 w-80 border-gray-01" />
+        <SignupForm />
+      </div>
+    </>
   );
 }
--- src/app/(plan)/plan/initial/page.tsx
+++ src/app/(plan)/plan/initial/page.tsx
@@ -1,7 +1,16 @@
+import Head from 'next/head';
+
 import PlanInitialForm from '@/components/PlanInitialForm';
 
 function Plan() {
-  return <PlanInitialForm />;
+  return (
+    <>
+      <Head>
+        <title>여행 계획 생성하기 - 트래블록</title>
+      </Head>
+      <PlanInitialForm />
+    </>
+  );
 }
 
 export default Plan;
