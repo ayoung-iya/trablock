5c1a688d5ee2584a8debc09c4172881670c1f8b1
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-30 03:17:20 +0900

    feat: 페이지네이션 관련 타입 추가

---
+++ src/apis/constants/pagination.type.ts
@@ -0,0 +1,33 @@
+export interface Pagination {
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
+
+export type PaginationParams = {
+  page: number;
+  size: number;
+  sort: 'createdAt,DESC' | 'popularity';
+};
