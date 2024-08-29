01ba4fdd67104dbd09e3b8f8e83924508b046228
ayoung-iya <rhaehfdl1879@naver.com>
2024-08-30 03:40:03 +0900

    fix: 타입 변경

--- src/apis/useArticle/usePutArticle.ts
+++ src/apis/useArticle/usePutArticle.ts
@@ -1,13 +1,13 @@
 import { QueryClient, useMutation } from '@tanstack/react-query';
 
-import { ArticleFormData } from './article.type';
-import ArticleService from './fetch';
+import { ArticleInitial } from '@/apis/useArticle/article.type';
+import ArticleService from '@/apis/useArticle/fetch';
 
 export default function usePutArticle(articleId: string) {
   const queryClient = new QueryClient();
 
   return useMutation({
-    mutationFn: (data: ArticleFormData) => ArticleService.putArticle(articleId, data),
+    mutationFn: (data: ArticleInitial) => ArticleService.putArticle(articleId, data),
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['trablock', 'article', 'useGetArticle', articleId] })
   });
 }
