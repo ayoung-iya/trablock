8f6f7b33ff27369f410fa5f6e0d6148e82587bd8
ayoung-iya <rhaehfdl1879@naver.com>
2024-09-13 02:56:22 +0900

    feat: article schedule 관련 type 추가

--- src/apis/useArticle/article.type.ts
+++ src/apis/useArticle/article.type.ts
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import { Category, Transport } from '@/libs/types/commonPlanType';
+
 export interface CityInfo {
   placeId: string;
   address: string;
@@ -30,4 +34,55 @@ export interface InitialArticle extends Pick<Article, 'title' | 'locations' | 't
 export interface InitialArticleRawData
   extends Pick<Article, 'title' | 'locations' | 'startAt' | 'endAt' | 'expense' | 'travelCompanion' | 'travelStyles'> {
   expense?: string;
+}
+
+export type Dtype = 'GENERAL' | 'TRANSPORT' | 'ETC';
+
+export interface Schedule {
+  scheduleId?: number;
+  visitedDate: string; // yyyy-mm-dd
+  visitedTime: string; // yyyy-mm-dd'T'HH-MM-ss.SSSZ
+  sortOrder: number;
+  category: Category;
+  durationTime: string; // yyyy-mm-dd'T'HH-MM-ss.SSSZ
+  expense: string;
+  memo?: string;
+  dtype: Dtype;
+  scheduleGeneral?: {
+    placeName: string;
+    googleMapPlaceId: string;
+    googleMapLatitude: number;
+    googleMapLongitude: number;
+    googleMapAddress: string;
+    googleMapPhoneNumber: string;
+    googleMapHomePageUrl: string;
+  };
+  scheduleTransport?: {
+    transportation: Transport;
+    startPlaceName: string;
+    googleMapStartPlaceAddress: string;
+    googleMapStartLatitude: number;
+    googleMapStartLongitude: number;
+    endPlaceName: string;
+    googleMapEndPlaceAddress: string;
+    googleMapEndLatitude: number;
+    googleMapEndLongitude: number;
+  };
+  scheduleEtc?: {
+    placeName: string;
+  };
+}
+
+export interface ScheduleDetail {
+  reviewId?: number;
+  isEditable: boolean;
+  schedules: Schedule[];
+}
+
+export interface ScheduleWithKey extends Schedule {
+  key: string;
+}
+
+export interface TabContentProps extends React.HTMLAttributes<HTMLButtonElement> {
+  schedule: ScheduleWithKey;
 }
