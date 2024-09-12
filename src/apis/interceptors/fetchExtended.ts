522b1c3a225a61a6cb5ab4f7d5a640119153d415
ayoung-iya <rhaehfdl1879@naver.com>
2024-09-13 02:27:51 +0900

    refactor: 공통 인터셉터에서 Content-type을 상수로 선언 및 적용

--- src/apis/interceptors/fetchExtended.ts
+++ src/apis/interceptors/fetchExtended.ts
@@ -12,12 +12,7 @@ interface ApiResponse<T> {
   error?: CustomError;
 }
 
-const defaultApiOptions = {
-  baseUrl: API_URL.API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
+const JSON_HEADERS = Object.freeze({ 'Content-Type': 'application/json' });
 
 const returnFetchThrowingErrorByStatusCode: ReturnFetch = (args) =>
   returnFetch({
@@ -65,6 +60,7 @@ const returnFetchAddAuthTokenInHeader: ReturnFetch = (args) =>
           ...options,
           headers: {
             ...options?.headers,
+            ...args?.headers,
             'authorization-token': token
           }
         };
@@ -112,14 +108,20 @@ const returnFetchJson = (args?: ReturnFetchDefaultOptions) => {
 
 export const fetchExtended = returnFetchJson({
   fetch: returnFetchThrowingErrorByStatusCode({
-    fetch: returnFetchHandleNotFound(defaultApiOptions)
+    fetch: returnFetchHandleNotFound({
+      baseUrl: API_URL.API_BASE_URL,
+      headers: JSON_HEADERS
+    })
   })
 });
 
 export const fetchExtendedWithAuthToken = returnFetchJson({
   fetch: returnFetchThrowingErrorByStatusCode({
-    fetch: returnFetchAddAuthTokenInHeader({
-      fetch: returnFetchHandleNotFound(defaultApiOptions)
+    fetch: returnFetchHandleNotFound({
+      fetch: returnFetchAddAuthTokenInHeader({
+        baseUrl: API_URL.API_BASE_URL,
+        headers: JSON_HEADERS
+      })
     })
   })
 });
