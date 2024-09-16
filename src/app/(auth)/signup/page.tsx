c0ee857a3ec14f20f9c52682ba4bc0b493dda4f7
ayoung-iya <rhaehfdl1879@naver.com>
2024-09-17 01:25:36 +0900

    refactor: 이벤트 핸들러에서 returnURL로 routing, 로그인 관련 페이지 서로 이동시 returnURL 유지되도록 설정

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -10,14 +10,22 @@ export const metadata: Metadata = {
   title: PAGE_TITLES.signup
 };
 
-export default function signup() {
+interface SignUp {
+  searchParams: {
+    returnURL?: string;
+  };
+}
+
+export default function signup({ searchParams: { returnURL } }: SignUp) {
   return (
     <div className="flex-col-center pb-32 pt-32">
-      <Logo width={123} height={32} />
+      <Link href="/">
+        <Logo width={123} height={32} />
+      </Link>
       <p className="font-signin-1 mt-5">
         아이디가 있으신가요?
         <span className="text-primary-01">
-          <Link href="/login"> 로그인</Link>
+          <Link href={{ pathname: '/login', query: { returnURL } }}> 로그인</Link>
         </span>
       </p>
       <p className="font-signin-2 mt-9 text-gray-01">SNS 계정으로 회원가입</p>
