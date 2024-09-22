facee97841035e75cb769a946bc77879e446e8c1
ayoung-iya <rhaehfdl1879@naver.com>
2024-09-23 02:58:45 +0900

    feat: 사진 위주의 여행 카드 합성 컴포넌트로 구현
    랜딩페이지와 프로필페이지에 비슷한 여행 카드가 존재. 레이아웃과
    내부콘텐츠의 차이가 존재하여 다음과 같이 구성.

---
+++ src/components/card/TravelPreviewCard.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+import Image from 'next/image';
+
+import { Article } from '@/apis/useArticle/article.type';
+import profileDefault from '@/icons/profile-default.svg?url';
+
+interface TravelPreviewCardProps extends Omit<Article, 'bookmarkCount' | 'isBookmarked' | 'isEditable' | 'locations'> {
+  cities: string[];
+}
+
+function CardTitle({ children }: React.PropsWithChildren) {
+  return <span className="font-subtitle-2 md:font-subtitle-2 text-white-01">{children}</span>;
+}
+
+function CardInfo({ children }: React.PropsWithChildren) {
+  return <span className="font-subtitle-3 text-gray-02">{children}</span>;
+}
+
+function CardProfile({ profileImgUrl, name }: Pick<TravelPreviewCardProps, 'profileImgUrl' | 'name'>) {
+  return (
+    <div className="flex-row-center gap-2">
+      <Image
+        src={profileImgUrl || profileDefault}
+        alt={`${name} 프로필`}
+        width={32}
+        height={32}
+        className="rounded-full bg-gray-02"
+      />
+      <span className="font-caption-2 text-white-01">{name}</span>
+    </div>
+  );
+}
+
+function CardMain({ children }: React.PropsWithChildren) {
+  return (
+    <div className="relative h-60 min-w-72 overflow-hidden rounded-[10px] bg-gray-02 md:h-64 lg:h-[220px] lg:min-w-[285px]">
+      <div className="absolute bottom-0 z-10 h-3/5 w-full bg-gradient-to-t from-black/60 to-transparent lg:h-3/4" />
+      {children}
+    </div>
+  );
+}
+
+function CardMainSquare({ children }: React.PropsWithChildren) {
+  return (
+    <div className="relative aspect-square size-full overflow-hidden rounded-[10px] bg-gray-02">
+      <div className="absolute bottom-0 z-10 h-2/4 w-full bg-gradient-to-t from-black/60 to-transparent" />
+      {children}
+    </div>
+  );
+}
+
+const TravelPreviewCard = Object.assign(CardMain, {
+  MainSquare: CardMainSquare,
+  Title: CardTitle,
+  Info: CardInfo,
+  Profile: CardProfile
+});
+
+export default TravelPreviewCard;
