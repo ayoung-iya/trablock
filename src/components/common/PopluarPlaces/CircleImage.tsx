f65d0da72aa572dda076967b9b89e89d95d96492
ayoung-iya <rhaehfdl1879@naver.com>
2024-11-12 01:28:18 +0900

    refactor: 불필요한 이벤트핸들러 제거, css 수정

--- src/components/common/PopluarPlaces/CircleImage.tsx
+++ src/components/common/PopluarPlaces/CircleImage.tsx
@@ -6,15 +6,13 @@ interface CircleImageProps {
   className?: string;
   imageUrl: StaticImageData;
   text: string;
-  onClick?: () => void;
 }
 
-export default function CircleImage({ className, imageUrl, text, onClick }: CircleImageProps) {
+export default function CircleImage({ className, imageUrl, text }: CircleImageProps) {
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <div className={`flex items-center justify-center ${className}`} onClick={onClick}>
-      <div className="relative flex size-[120px] cursor-pointer items-center justify-center rounded-full bg-gray-300  md:size-[140px]">
-        <Image src={imageUrl} alt="profile" className=" size-[120px] rounded-full brightness-50 md:size-[140px]" />
+    <div className={`flex items-center justify-center ${className}`}>
+      <div className="relative flex size-[120px] cursor-pointer items-center justify-center overflow-hidden rounded-full bg-gray-300 md:size-[140px]">
+        <Image src={imageUrl} alt="profile" className="brightness-50" width={140} height={140} />
         <div className="absolute inset-0 flex items-center justify-center">
           <span className="text-lg font-bold text-slate-50">{text}</span>
         </div>
